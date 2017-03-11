1d21303e2e5c6b7c9cf244595d9ef57338cd01d6
William Di Luigi <williamdiluigi@gmail.com>
2017-03-11 17:17:05 +0100

    Work in progress: taskrun

--- taskwizard/supervisor/supervisor.py
+++ taskwizard/supervisor/supervisor.py
@@ -1,16 +1,21 @@
-"""Naval Fate.
+"""Task run.
 
 Usage:
-  taskrun <task-folder> <submission> [--maxproc=<maxproc>]
+  taskrun <task-folder> <testcase-id> <slot:submission>... [--maxproc=<maxproc>]
   taskrun (-h | --help)
 
 Options:
+  task-folder          Path to a task definition folder.
+  testcase-id          Testcase ID.
+  slot:submission      Slot/submission pair (example: player1:sol/play.cpp)
   -h --help            Show this screen.
   --maxproc=<maxproc>  Max num of processes [default: 20].
 """
 
 import logging
 import os
+import shutil
+import tempfile
 
 from docopt import docopt
 
@@ -20,6 +25,12 @@ logger = logging.getLogger("taskrun")
 
 def main():
     args = docopt(__doc__)
+    print(args)
 
     if not os.path.isdir(args["<task-folder>"]):
         logger.critical("%s should be a folder", args["<task-folder>"])
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        shutil.copytree(args["<task-folder>"], tmpdirname)
+
+    pass
