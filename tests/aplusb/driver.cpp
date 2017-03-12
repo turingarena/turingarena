8457d644bd5d1a189d70784d48a42788aae109af
Massimo Cairo <cairomassimo@gmail.com>
2017-03-12 21:27:21 +0100

    Make 'aplusb' driver score the solution.

--- tests/aplusb/driver.cpp
+++ tests/aplusb/driver.cpp
@@ -2,10 +2,12 @@
 
 int evaluate(int A, int B) {
   int solution = algorithm_start("solution");
-
   int result = call_sum(solution, A, B);
   algorithm_kill(solution);
 
-  fprintf(stderr, "Result: %d\n", result);
-  return 0;
+  if(result == A+B) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
