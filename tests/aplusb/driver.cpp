25e9c18a8c76cd967abb1abd956175953e57f299
William Di Luigi <williamdiluigi@gmail.com>
2017-03-12 16:26:50 +0100

    Fix compilation error in generated code

--- tests/aplusb/driver.cpp
+++ tests/aplusb/driver.cpp
@@ -1,12 +1,11 @@
 #include "driver.h"
 
-int sum(int A, int B);
-
 int evaluate(int A, int B) {
   int solution = algorithm_start("solution");
-  set_active_algorithm(solution);
-  int result = sum(A, B);
+
+  int result = call_sum(solution, A, B);
   algorithm_kill(solution);
+
   fprintf(stderr, "Result: %d\n", result);
-  exit(0);
+  return 0;
 }
