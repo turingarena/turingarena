8a5c7d3d095b61ef179c4ac07069c12f498594f6
Massimo Cairo <cairomassimo@gmail.com>
2017-05-10 12:34:32 +0200

    Finish dropping of test cases.

--- taskwizard/definition/task.py
+++ taskwizard/definition/task.py
@@ -1,10 +1,9 @@
 from collections import OrderedDict
 
 from taskwizard.definition.declarations import named_definitions
-from taskwizard.definition.module import DriverDefinition
+from taskwizard.definition.module import ModuleDefinition
 from taskwizard.definition.interface import Interface, InterfaceDefinition
 from taskwizard.definition.syntax import AbstractSyntaxNode
-from taskwizard.definition.test_case import TestCaseDefinition
 
 
 class TaskDefinition(AbstractSyntaxNode):
@@ -13,27 +12,24 @@ class TaskDefinition(AbstractSyntaxNode):
     grammar = """
         task_definition =
         {
+        | module_definitions+:module_definition
         | interface_definitions+:interface_definition
-        | driver_definitions+:driver_definition
-        | test_case_definitions+:test_case_definition
         }* $ ;
     """
-    grammar_deps = lambda: [InterfaceDefinition, DriverDefinition, TestCaseDefinition]
+    grammar_deps = lambda: [ModuleDefinition, InterfaceDefinition]
 
     def __init__(self, ast):
         self.name = ast.name
         self.interface_definitions = ast.get("interface_definitions", [])
-        self.driver_definitions = ast.get("driver_definitions", [])
-        self.test_case_definitions = ast.get("test_case_definitions", [])
+        self.module_definitions = ast.get("module_definitions", [])
 
 
 class Task:
 
     def __init__(self, definition):
         self.definition = definition
-        #self.drivers = named_definitions(definition.drivers)
+        #self.modules = named_definitions(definition.modules)
         self.interfaces = OrderedDict(
                 (d.name, Interface(d))
                 for d in definition.interface_definitions
         )
-        #self.test_cases = named_definitions(definition.test_cases)
