6dc491f2cd642fabacae903312bb7164de1bd4bc
Massimo Cairo <cairomassimo@gmail.com>
2017-11-10 10:31:05 +0100

    Update test.sh to match changes.

--- tests/test_task/test.sh
+++ tests/test_task/test.sh
@@ -9,7 +9,7 @@ turingarena protocol --name test_challenge skeleton
 turingarena sandbox compile \
     --protocol test_challenge \
     --interface exampleinterface \
-    -x c++ -o solution solution.cpp
+    -x c++ -o entry entry.cpp
 
 turingarena problem --module test_challenge \
     goal --name goal evaluate
