ebabbefc99b726fa0e1fdd1b8a3bb395446dba2b
Massimo Cairo <cairomassimo@gmail.com>
2017-11-30 15:36:32 +0100

    Add "nofork" option to socat (fixes random failures).

--- turingarena/container/sshd.py
+++ turingarena/container/sshd.py
@@ -8,6 +8,6 @@ def serve_sshd(name):
         [
             "socat",
             f"UNIX-LISTEN:/var/run/turingarena/{name}/sshd.sock,fork,mode=0666",
-            f"EXEC:/usr/sbin/sshd -i -e -o PermitEmptyPasswords=yes -o PermitRootLogin=yes -o Protocol=2",
+            f"EXEC:/usr/sbin/sshd -i -e -o PermitEmptyPasswords=yes -o PermitRootLogin=yes -o Protocol=2,nofork",
         ]
     )
