e18777b1bbf8dcae609953e1d5f083c50c9df996
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-02-21 19:45:32 +0100

    unified types test

--- turingarena/protocol/tests/types_test.py
+++ turingarena/protocol/tests/types_test.py
@@ -1,88 +1,7 @@
-from turingarena.protocol.tests.util import cpp_implementation, python_implementation
+from turingarena.protocol.tests.util import algorithm
 
 
-def test_valid_types_python():
-    protocol_text = """
-        interface types_valid {
-            var int i;
-            var int[] ia;
-            var int[][] iaa;
-        
-            function get_i() -> int;
-            function get_ia(int j) -> int;
-            function get_iaa(int j, int k) -> int;
-        
-            main {
-                input i;
-                alloc ia, iaa : i;
-                for(j : i) {
-                    input ia[j];
-                    alloc iaa[j] : ia[j];
-                    for(k : ia[j]) {
-                        input iaa[j][k];
-                    }
-                }
-        
-                var int o;
-                var int[] oa;
-                var int[][] oaa;
-        
-                call get_i() -> o;
-                alloc oa, oaa : i;
-                for(j : i) {
-                    call get_ia(j) -> oa[j];
-                    alloc oaa[j] : ia[j];
-                    for(k : ia[j]) {
-                        call get_iaa(j, k) -> oaa[j][k];
-                    }
-                }
-        
-                output o;
-                for(j : i) {
-                    output oa[j];
-                    for(k : ia[j]) {
-                        output oaa[j][k];
-                    }
-                }
-            }
-        }
-    """
-    # requires 0 identation
-    python_source_text = """
-from __main__ import i, ia, iaa
-
-def get_i():
-    return i
-
-def get_ia(j):
-    return ia[j]
-
-def get_iaa(j, k):
-    return iaa[j][k]
-    """
-
-    iaa = [
-        [1, 2, 3],
-        [],
-        [4, 5],
-    ]
-    ia = [len(x) for x in iaa]
-    i = len(ia)
-
-    with python_implementation(
-            protocol_text=protocol_text,
-            source_text=python_source_text,
-            interface_name="types_valid",
-    ) as py_impl:
-        with py_impl.run(i=i, ia=ia, iaa=iaa) as (process, p):
-            assert i == p.get_i()
-            for j in range(i):
-                assert ia[j] == p.get_ia(j)
-                for k in range(ia[j]):
-                    assert iaa[j][k] == p.get_iaa(j, k)
-
-
-def test_valid_types_cpp():
+def test_valid_types():
     protocol_text = """
         interface types_valid {
             var int i;
@@ -129,21 +48,35 @@ def test_valid_types_cpp():
         }
     """
 
-    cpp_source_text = """
-        int i, *ia, **iaa;
-    
-        int get_i() {
-            return i;
-        }
-        
-        int get_ia(int j) {
-            return ia[j];
-        }
-        
-        int get_iaa(int j, int k) {
-            return iaa[j][k];
-        }
-    """
+    source_mapping = {
+            'python' : """if True:
+                from __main__ import i, ia, iaa
+
+                def get_i():
+                    return i
+
+                def get_ia(j):
+                    return ia[j]
+
+                def get_iaa(j, k):
+                    return iaa[j][k]
+                    """,
+            'c++' : """
+                int i, *ia, **iaa;
+            
+                int get_i() {
+                    return i;
+                }
+                
+                int get_ia(int j) {
+                    return ia[j];
+                }
+                
+                int get_iaa(int j, int k) {
+                    return iaa[j][k];
+                }
+            """ }
+
     iaa = [
         [1, 2, 3],
         [],
@@ -152,14 +85,16 @@ def test_valid_types_cpp():
     ia = [len(x) for x in iaa]
     i = len(ia)
 
-    with cpp_implementation(
-            protocol_text=protocol_text,
-            source_text=cpp_source_text,
-            interface_name="types_valid",
-    ) as cpp_impl:
-        with cpp_impl.run(i=i, ia=ia, iaa=iaa) as (process, p):
-            assert i == p.get_i()
-            for j in range(i):
-                assert ia[j] == p.get_ia(j)
-                for k in range(ia[j]):
-                    assert iaa[j][k] == p.get_iaa(j, k)
+    for lang, source in source_mapping.items():
+        with algorithm(
+                protocol_text=protocol_text,
+                language=lang,
+                source_text=source,
+                interface_name="types_valid",
+        ) as impl:
+            with impl.run(i=i, ia=ia, iaa=iaa) as (process, p):
+                assert i == p.get_i()
+                for j in range(i):
+                    assert ia[j] == p.get_ia(j)
+                    for k in range(ia[j]):
+                        assert iaa[j][k] == p.get_iaa(j, k)
