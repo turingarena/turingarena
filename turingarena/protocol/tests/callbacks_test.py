9cac086653d1d2db15bd6abe08f1761ad23a1c89
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-02-21 20:47:19 +0100

    improved callbascks_test

--- turingarena/protocol/tests/callbacks_test.py
+++ turingarena/protocol/tests/callbacks_test.py
@@ -2,121 +2,119 @@ import pytest
 
 from turingarena.protocol.exceptions import ProtocolError
 from turingarena.protocol.module import ProtocolSource
-from turingarena.protocol.tests.util import cpp_implementation, callback_mock, python_implementation
+from turingarena.protocol.tests.util import cpp_implementation, callback_mock, algorithm, python_implementation
 
 
 def test_callback_no_arguments_cpp():
-    with cpp_implementation(
-            protocol_text="""
-                interface callback_no_arguments {
-                    callback callback_no_arguments() {}
-                    function test();
-                    main {
-                        call test();
-                    }
+    protocol_text = """
+            interface callback_no_arguments {
+                callback callback_no_arguments() {}
+                function test();
+                main {
+                    call test();
                 }
-            """,
-            source_text="""
+            }
+        """
+
+    source_mapping = {
+            'c++' : """
                 void callback_no_arguments();
                 void test() {
                     callback_no_arguments();
                     callback_no_arguments();
                 }
             """,
-            interface_name="callback_no_arguments",
-    ) as impl:
-        with impl.run() as (process, p):
-            calls = []
-            callback_no_arguments = callback_mock(calls)
-            p.test(callback_no_arguments=callback_no_arguments)
-
-            assert calls == [
-                (callback_no_arguments, ()),
-                (callback_no_arguments, ()),
-            ]
-
-def test_callback_no_arguments_python():
-    with python_implementation(
-            protocol_text="""
-                interface callback_no_arguments {
-                    callback callback_no_arguments() {}
-                    function test();
-                    main {
-                        call test();
-                    }
-                }
-            """,
-            # requires no indentation
-            source_text="""if True:
+            'python' : """if True:
                 from __main__ import callback_no_arguments
                 
                 def test():
                     callback_no_arguments()
                     callback_no_arguments()
             """,
-            interface_name="callback_no_arguments",
-    ) as impl:
-        with impl.run() as (process, p):
-            calls = []
-            callback_no_arguments = callback_mock(calls)
-            p.test(callback_no_arguments=callback_no_arguments)
+        }
 
-            assert calls == [
-                (callback_no_arguments, ()),
-                (callback_no_arguments, ()),
-            ]
+    for lang, source in source_mapping.items():
+        with algorithm(
+                protocol_text=protocol_text,
+                language=lang,
+                source_text=source,
+                interface_name="callback_no_arguments",
+        ) as impl:
+            with impl.run() as (process, p):
+                calls = []
+                callback_no_arguments = callback_mock(calls)
+                p.test(callback_no_arguments=callback_no_arguments)
 
-def test_callback_with_arguments_cpp():
-    with cpp_implementation(
-            protocol_text="""
-                interface callback_with_arguments {
-                    callback callback_with_arguments(int a, int b) {
-                        output a, b;
-                    }
-                    function test();
-                    main {
-                        call test();
-                    }
-                }
-            """,
-            source_text="""
-                void callback_with_arguments(int a, int b);
-                void test() {
-                    callback_with_arguments(1, 2);
-                    callback_with_arguments(3, 4);
-                }
-            """,
-            interface_name="callback_with_arguments",
-    ) as impl:
-        with impl.run() as (process, p):
-            calls = []
-            callback_with_arguments = callback_mock(calls)
-            p.test(callback_with_arguments=callback_with_arguments)
+                assert calls == [
+                    (callback_no_arguments, ()),
+                    (callback_no_arguments, ()),
+                ]
 
-            assert calls == [
-                (callback_with_arguments, (1, 2)),
-                (callback_with_arguments, (3, 4)),
-            ]
+def test_callback_with_arguments():
+    protocol_text="""
+        interface callback_with_arguments {
+            callback callback_with_arguments(int a, int b) {
+                output a, b;
+            }
+            function test();
+            main {
+                call test();
+            }
+        }
+    """
 
+    source_mapping = {
+        'c++' : """
+            void callback_with_arguments(int a, int b);
+            void test() {
+                callback_with_arguments(1, 2);
+                callback_with_arguments(3, 4);
+            }
+        """,
+        'python' : """if True:
+            from __main__ import callback_with_arguments
+            def test():
+                callback_with_arguments(1, 2)
+                callback_with_arguments(3, 4)
+        """,
+        }
 
-def test_callback_return_value_cpp():
-    with cpp_implementation(
-            protocol_text="""
-                interface callback_return_value {
-                    callback callback_return_value(int a) -> int {
-                        output a;
-                        flush;
-                        var int b;
-                        input b;
-                        return b;
-                    }
-                    function test();
-                    main {
-                        call test();
-                    }
-                }
-            """,
-            source_text="""
+    for lang, source in source_mapping.items():
+        with algorithm(
+                protocol_text=protocol_text,
+                language=lang,
+                source_text=source,
+                interface_name="callback_with_arguments",
+        ) as impl:
+            with impl.run() as (process, p):
+                calls = []
+                callback_with_arguments = callback_mock(calls)
+                p.test(callback_with_arguments=callback_with_arguments)
+
+                assert calls == [
+                    (callback_with_arguments, (1, 2)),
+                    (callback_with_arguments, (3, 4)),
+                ]
+
+def test_callback_return_value():
+    protocol_text="""
+        interface callback_return_value {
+            callback callback_return_value(int a) -> int {
+                output a;
+                flush;
+                var int b;
+                input b;
+                return b;
+            }
+            function test();
+            main {
+                call test();
+            }
+        }
+    """
+
+    source_mapping = {
+            'c++' : """
                 #include <cassert>
                 int callback_return_value(int a);
                 void test() {
@@ -124,17 +122,30 @@ def test_callback_return_value_cpp():
                     assert(callback_return_value(3) == 4);
                 }
             """,
-            interface_name="callback_return_value",
-    ) as impl:
-        with impl.run() as (process, p):
-            calls = []
-            callback_return_value = callback_mock(calls, [2, 4])
-            p.test(callback_return_value=callback_return_value)
+            'python' : """if True:
+                from __main__ import callback_return_value
+                def test():
+                    assert callback_return_value(1) == 2
+                    assert callback_return_value(3) == 4
+            """
+        }
 
-            assert calls == [
-                (callback_return_value, (1,)),
-                (callback_return_value, (3,)),
-            ]
+    for lang, source in source_mapping.items():
+        with algorithm(
+                protocol_text=protocol_text,
+                language=lang,
+                source_text=source,
+                interface_name="callback_return_value",
+        ) as impl:
+            with impl.run() as (process, p):
+                calls = []
+                callback_return_value = callback_mock(calls, [2, 4])
+                p.test(callback_return_value=callback_return_value)
+
+                assert calls == [
+                    (callback_return_value, (1,)),
+                    (callback_return_value, (3,)),
+                ]
 
 
 def test_callback_return_type_not_scalar():
