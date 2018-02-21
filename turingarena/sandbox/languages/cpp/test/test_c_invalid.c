209c498d2941a9b8615baa21650ab11af0b4f430
Alessandro Righi <alerighi4@gmail.com>
2018-02-21 18:59:43 +0100

    Added some tests

---
+++ turingarena/sandbox/languages/cpp/test/test_c_invalid.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() 
+{
+	/* test open file (should fail) */
+	FILE *fp = fopen("Makefile", "r");
+}
---
+++ turingarena/sandbox/languages/cpp/test/test_cpp_invalid.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() 
+{
+	/* should fail */
+	ifstream in("Makefile");
+}
