177294baa4178877c3542062c74e6c0180376460
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-02-21 09:37:41 +0100

    add python test for functions

--- turingarena/protocol/tests/functions_test.py
+++ turingarena/protocol/tests/functions_test.py
@@ -2,10 +2,10 @@ import pytest
 
 from turingarena.protocol.exceptions import ProtocolError
 from turingarena.protocol.module import ProtocolSource
-from turingarena.protocol.tests.util import cpp_implementation
+from turingarena.protocol.tests.util import cpp_implementation, python_implementation
 
 
-def test_function_no_arguments():
+def test_function_no_arguments_cpp():
     with cpp_implementation(
             protocol_text="""
                 interface function_no_arguments {
@@ -24,8 +24,28 @@ def test_function_no_arguments():
         with impl.run() as p:
             p.function_no_arguments()
 
+def test_function_no_arguments_python():
+    with python_implementation(
+            protocol_text="""
+                interface function_no_arguments {
+                    function function_no_arguments();
+                    main {
+                        call function_no_arguments();
+                    }
+                }
+            """,
+            # requires 0 indentation
+            source_text="""
+def function_no_arguments():
+    pass
+            """,
+            interface_name="function_no_arguments",
+    ) as impl:
+        with impl.run() as p:
+            p.function_no_arguments()
 
-def test_function_with_arguments():
+
+def test_function_with_arguments_cpp():
     with cpp_implementation(
             protocol_text="""
                 interface function_with_arguments {
@@ -48,8 +68,28 @@ def test_function_with_arguments():
         with impl.run() as p:
             p.function_with_arguments(1, 2)
 
+def test_function_with_arguments_python():
+    with python_implementation(
+            protocol_text="""
+                interface function_with_arguments {
+                    function function_with_arguments(int a, int b);
+                    main {
+                        var int a, b;
+                        input a, b;
+                        call function_with_arguments(a, b);
+                    }
+                }
+            """,
+            source_text="""
+def function_with_arguments(a,b):
+    assert a == 1 and b == 2
+            """,
+            interface_name="function_with_arguments",
+    ) as impl:
+        with impl.run() as p:
+            p.function_with_arguments(1, 2)
 
-def test_function_return_value():
+def test_function_return_value_cpp():
     with cpp_implementation(
             protocol_text="""
                 interface function_return_value {
@@ -74,6 +114,28 @@ def test_function_return_value():
         with impl.run() as p:
             assert p.function_return_value(1) == 2
 
+def test_function_return_value_python():
+    with python_implementation(
+            protocol_text="""
+                interface function_return_value {
+                    function function_return_value(int a) -> int;
+                    main {
+                        var int a, b;
+                        input a;
+                        call function_return_value(a) -> b;
+                        output b;
+                    }
+                }
+            """,
+            source_text="""
+def function_return_value(a):
+    assert a == 1
+    return 2
+            """,
+            interface_name="function_return_value",
+    ) as impl:
+        with impl.run() as p:
+            assert p.function_return_value(1) == 2
 
 def test_function_return_type_not_scalar():
     protocol_text = """
