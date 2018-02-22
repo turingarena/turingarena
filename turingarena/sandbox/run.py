54ab3e582603ca57ae8863a0c0c0986b5b53ff71
Massimo Cairo <cairomassimo@gmail.com>
2018-02-22 15:51:53 +0100

    Reorganize slightly code in sandbox/run.py

--- turingarena/sandbox/run.py
+++ turingarena/sandbox/run.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 import tempfile
-from contextlib import ExitStack
+from contextlib import ExitStack, contextmanager
 from threading import Thread
 
 from turingarena.sandbox.client import ProcessConnection
@@ -21,15 +21,44 @@ class SandboxProcess:
         self.executable = executable
         self.sandbox_dir = sandbox_dir
 
+        logger.debug("sandbox folder: %s", sandbox_dir)
+
         self.downward_pipe_name = os.path.join(sandbox_dir, "downward.pipe")
         self.upward_pipe_name = os.path.join(sandbox_dir, "upward.pipe")
         self.error_pipe_name = os.path.join(sandbox_dir, "error.pipe")
         self.wait_pipe_name = os.path.join(sandbox_dir, "wait.pipe")
 
-        self.os_process = None
+        self.create_pipes()
 
-        logger.debug("sandbox folder: %s", sandbox_dir)
+        print(self.sandbox_dir)
+        sys.stdout.close()
 
+        self.run()
+
+    def run(self):
+        wait_thread = None
+        with self.open_connection() as connection:
+            try:
+                with self.executable.run(connection) as p:
+                    def wait():
+                        self.wait_for_wait_pipe()
+                        p.kill()
+
+                    wait_thread = Thread(target=wait)
+                    wait_thread.start()
+            except AlgorithmRuntimeError as e:
+                logger.exception(e)
+                connection.error_pipe.write(str(e))
+        if wait_thread:
+            wait_thread.join()
+
+    def wait_for_wait_pipe(self):
+        logger.debug("opening wait pipe...")
+        with open(self.wait_pipe_name, "w"):
+            pass
+        logger.debug("wait pipe opened, terminating...")
+
+    def create_pipes(self):
         logger.debug("creating pipes...")
         os.mkfifo(self.downward_pipe_name)
         os.mkfifo(self.upward_pipe_name)
@@ -37,43 +66,22 @@ class SandboxProcess:
         os.mkfifo(self.wait_pipe_name)
         logger.debug("pipes created")
 
-        print(self.sandbox_dir)
-        sys.stdout.close()
+    @contextmanager
+    def open_connection(self):
+        with ExitStack() as stack:
+            logger.debug("opening downward pipe...")
+            downward_pipe = stack.enter_context(open(self.downward_pipe_name, "r"))
+            logger.debug("opening upward pipe...")
+            upward_pipe = stack.enter_context(open(self.upward_pipe_name, "w"))
+            logger.debug("opening error pipe...")
+            error_pipe = stack.enter_context(open(self.error_pipe_name, "w"))
+            logger.debug("pipes opened")
 
-        wait_thread = None
-        try:
-            with ExitStack() as stack:
-                logger.debug("opening downward pipe...")
-                downward_pipe = stack.enter_context(open(self.downward_pipe_name, "r"))
-                logger.debug("opening upward pipe...")
-                upward_pipe = stack.enter_context(open(self.upward_pipe_name, "w"))
-                logger.debug("opening error pipe...")
-                error_pipe = stack.enter_context(open(self.error_pipe_name, "w"))
-                logger.debug("pipes opened")
-
-                connection = ProcessConnection(
-                    downward_pipe=downward_pipe,
-                    upward_pipe=upward_pipe,
-                    error_pipe=error_pipe,
-                )
-
-                try:
-                    with self.executable.run(connection) as p:
-                        def wait():
-                            logger.debug("opening wait pipe...")
-                            with open(self.wait_pipe_name, "w"):
-                                pass
-                            logger.debug("wait pipe opened, terminating...")
-                            p.kill()
-
-                        wait_thread = Thread(target=wait)
-                        wait_thread.start()
-                except AlgorithmRuntimeError as e:
-                    logger.exception(e)
-                    error_pipe.write(str(e))
-        finally:
-            if wait_thread:
-                wait_thread.join()
+            yield ProcessConnection(
+                downward_pipe=downward_pipe,
+                upward_pipe=upward_pipe,
+                error_pipe=error_pipe,
+            )
 
 
 def sandbox_run(algorithm_dir):
