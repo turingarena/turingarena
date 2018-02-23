18e9380a55b552260a78590777a41580846cc6ad
Massimo Cairo <cairomassimo@gmail.com>
2018-02-23 11:48:22 +0100

    Revert "Use a sandbox server instead of spawning processes."
    This reverts commit 224158ab0e3474731fcb0c574fd187693873a37b.

--- turingarena/sandbox/connection.py
+++
@@ -1,5 +0,0 @@
-from turingarena.common import ImmutableObject
-
-
-class ProcessConnection(ImmutableObject):
-    __slots__ = ["downward_pipe", "upward_pipe", "error_pipe"]
--- turingarena/sandbox/run.py
+++ turingarena/sandbox/run.py
@@ -1,12 +1,11 @@
 import logging
 import os
-import shutil
+import sys
 import tempfile
 from contextlib import ExitStack, contextmanager
-from tempfile import mkdtemp
 from threading import Thread
 
-from turingarena.sandbox.connection import ProcessConnection
+from turingarena.sandbox.client import ProcessConnection
 from turingarena.sandbox.exceptions import AlgorithmRuntimeError
 from turingarena.sandbox.executables import load_executable
 
@@ -30,7 +29,11 @@ class SandboxProcess:
         self.wait_pipe_name = os.path.join(sandbox_dir, "wait.pipe")
 
         self.create_pipes()
-        self.waited = False
+
+        print(self.sandbox_dir)
+        sys.stdout.close()
+
+        self.run()
 
     def run(self):
         wait_thread = None
@@ -39,15 +42,13 @@ class SandboxProcess:
                 with self.executable.run(connection) as p:
                     def wait():
                         self.wait_for_wait_pipe()
-                        self.waited = True
                         p.kill()
 
                     wait_thread = Thread(target=wait)
                     wait_thread.start()
             except AlgorithmRuntimeError as e:
                 logger.exception(e)
-                if not self.waited:
-                    connection.error_pipe.write(str(e))
+                connection.error_pipe.write(str(e))
         if wait_thread:
             wait_thread.join()
 
@@ -83,39 +84,9 @@ class SandboxProcess:
             )
 
 
-class SandboxServer:
-    def __init__(self):
-        self.server_dir = mkdtemp(prefix="turingarena_sandbox_server_")
+def sandbox_run(algorithm_dir):
+    prefix = "turingarena_sandbox_"
 
-        self.request_pipe_name = os.path.join(self.server_dir, "sandbox_server_request.pipe")
-        self.response_pipe_name = os.path.join(self.server_dir, "sandbox_server_response.pipe")
-
-        os.mkfifo(self.request_pipe_name)
-        os.mkfifo(self.response_pipe_name)
-
-        # FIXME: should not be a daemon thread
-        self.thread = Thread(target=self.serve_sandboxes, daemon=True)
-        self.thread.start()
-
-    @contextmanager
-    def sandbox_run(self, algorithm_dir):
-        executable = load_executable(algorithm_dir)
-
-        prefix = "algorithm_"
-        with tempfile.TemporaryDirectory(prefix=prefix, dir=self.server_dir) as sandbox_dir:
-            process = SandboxProcess(executable=executable, sandbox_dir=sandbox_dir)
-            yield sandbox_dir
-            process.run()
-
-    def serve_sandboxes(self):
-        try:
-            while True:
-                with open(self.request_pipe_name) as f:
-                    algorithm_dir = f.read()
-                    if not algorithm_dir:
-                        raise Exception("stop")
-                with self.sandbox_run(algorithm_dir) as sandbox_dir:
-                    with open(self.response_pipe_name, "w") as f:
-                        f.write(sandbox_dir)
-        finally:
-            shutil.rmtree(self.server_dir)
+    executable = load_executable(algorithm_dir)
+    with tempfile.TemporaryDirectory(prefix=prefix) as sandbox_dir:
+        SandboxProcess(executable=executable, sandbox_dir=sandbox_dir)
