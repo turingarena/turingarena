f53d8060032cbd042d2a08310e27eae5ef0a9d62
alerighi <alessandro.righi@outlook.it>
2018-03-08 21:37:54 +0100

    JavaScript: fix solution to triangle_dynamic_programming

--- turingarena/problems/triangle_dynamic_programming/solutions/correct.js
+++ turingarena/problems/triangle_dynamic_programming/solutions/correct.js
@@ -5,11 +5,11 @@ function max(a, b) {
 function find_best_sum() {
     dyn = new Array(N+1);
     for (var i = 0; i <= N; i++)
-        dyn[i] = Array(N+1);
+        dyn[i] = new Array(N+1);
     for (var i = 0; i <= N; i++)
         dyn[N][i] = 0;
-    for(var i = N-1; i >= 0; i--)
-        for(var j = 0; j < i; j++)
+    for (var i = N-1; i >= 0; i--)
+        for (var j = 0; j < i; j++)
             dyn[i][j] = V[i][j] + max(dyn[i+1][j], dyn[i+1][j+1]);
     return dyn[1][0];
 }
