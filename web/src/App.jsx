379acfb91b86a046eb095a2b25252df45d8ff06e
Massimo Cairo <cairomassimo@gmail.com>
2018-03-13 11:08:14 +0100

    web: Allow multiple submissions, show results.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'preact';
-import ReactModal from 'react-modal';
 import PropTypes from 'prop-types';
 import './App.css';
 
 
-const UploadView = ({ onSubmit }) => (
-  <form onSubmit={(e) => { e.preventDefault(); onSubmit(e.target); }}>
+const UploadView = ({ onSubmit, disabled }) => (
+  <form disabled={disabled} onSubmit={(e) => { e.preventDefault(); onSubmit(e.target); }}>
     <select name="problem">
       <option>turingarena.problems.sum_of_two_numbers</option>
       <option>turingarena.problems.max_in_sequence</option>
@@ -18,41 +17,57 @@ const UploadView = ({ onSubmit }) => (
       <option value="java">Java</option>
       <option value="javascript">Javascript</option>
     </select>
-    <button type="submit">Submit</button>
+    <button type="submit" disabled={disabled}>Submit</button>
   </form>
 );
 
 UploadView.propTypes = {
+  disabled: PropTypes.bool.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
-const submit = (form) => {
-  console.log(process.env.TURINGARENA_EVALUATE_ENDPOINT);
-  return fetch(process.env.TURINGARENA_EVALUATE_ENDPOINT, {
-    method: 'post',
-    body: new FormData(form),
-  }).then((response) => {
-    if (response.status !== 200) {
-      return Promise.reject(response);
-    }
-    return response.text();
-  });
+const submit = form => fetch(process.env.TURINGARENA_EVALUATE_ENDPOINT, {
+  method: 'post',
+  body: new FormData(form),
+}).then((response) => {
+  if (response.status !== 200) {
+    return Promise.reject(response);
+  }
+  return response.text();
+});
+
+const ResultView = ({ result }) => (
+  <pre>
+    {result.evaluation}
+  </pre>
+);
+
+ResultView.propTypes = {
+  result: PropTypes.shape({ evaluation: PropTypes.string.isRequired }).isRequired,
 };
 
 class SubmitView extends React.Component {
   state = {
-    result: null,
+    phase: 'pristine',
+    result: {},
   };
+
+  submit(t) {
+    this.setState({ phase: 'submitted', result: {} });
+    submit(t).then((r) => {
+      this.setState({ phase: 'resolved', result: { evaluation: r } });
+    }, (r) => {
+      this.setState({ phase: 'rejected', result: { error: r } });
+    });
+  }
 
   render() {
     return (
       <React.Fragment>
-        <UploadView onSubmit={t => submit(t).then(r => this.setState({ result: r }))} />
-        <ReactModal isOpen={!!this.state.result}>
-          <pre>
-            {this.state.result}
-          </pre>
-        </ReactModal>
+        <UploadView disabled={this.state.phase === 'submitted'} onSubmit={t => this.submit(t)} />
+        {this.state.phase === 'submitted' && <p>Evaluating... (may take up to one minute)</p>}
+        {this.state.phase === 'resolved' && <ResultView result={this.state.result} />}
+        {this.state.phase === 'rejected' && <pre>{this.state.result.error}</pre>}
       </React.Fragment>
     );
   }
