3661e2a34c5693bc6dff1694f3e2929b1c33d8d2
alerighi <alessandro.righi@outlook.it>
2018-03-22 15:24:57 +0100

    Add longest_common_subsequence example problem

---
+++ examples/longest_common_subsequence/evaluate.py
@@ -0,0 +1,59 @@
+import random
+
+def LCS_length(x, y):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:
+                c[i][j] = c[i-1][j-1] + 1
+            elif c[i-1][j] >= c[i][j-1]:
+                c[i][j] = c[i-1][j]
+            else:
+                c[i][j] = c[i][j-1]
+
+    return c[m][n]
+
+
+def is_valid_solution(x, y, sol):
+    if len(sol) != LCS_length(x, y):
+        return False
+    for c in sol:
+        try:
+            i = x.index(c)
+            j = y.index(c)
+        except ValueError:
+            return False
+        x = x[i+1:]
+        y = y[j+1:]
+    return True
+
+
+def evaluate_test_case(submission, N):
+    x = [
+        random.randint(0, 10)
+        for _ in range(N)
+    ]
+    y = [
+        random.randint(0, 10)
+        for _ in range(N)
+    ]
+    m = len(x)
+    n = len(y)
+    with submission.run(global_variables=dict(X=x, Y=y, M=m, N=n)) as process:
+        process.call.compute()
+        sol = [
+            process.call.element(i)
+            for i in range(process.call.length())
+        ]
+        return is_valid_solution(x, y, sol)
+
+
+def evaluate(submission):
+    for n in range(100, 1100, 100):
+        if evaluate_test_case(submission, n):
+            print(f'test case N = {n} correct')
+        else:
+            print(f'test case N = {i} wrong')
---
+++ examples/longest_common_subsequence/solutions/correct.cpp
@@ -0,0 +1,73 @@
+
+int M;
+
+int *X;
+
+int N;
+
+int *Y;
+
+int **b;
+int **c;
+int *seq;
+
+void build_sequence(int x, int i, int j) {
+    if (i == 0 || j == 0) {
+        return;
+    }
+
+    if (b[i][j] == 1) {
+        seq[x] = X[i - 1];
+        build_sequence(x - 1, i - 1, j - 1);
+    } else if (b[i][j] == 2) {
+        build_sequence(x, i - 1, j);
+    } else {
+        build_sequence(x, i, j - 1);
+    }
+}
+
+void compute() {
+    b = new int*[M+1];
+    c = new int*[M+1];
+
+    for (int i = 0; i <= M; i++) {
+        b[i] = new int[N+1];
+        c[i] = new int[N+1];
+    }
+
+    for (int i = 0; i <= M; i++) {
+        c[i][0] = 0;
+    }
+
+    for (int j = 0; j <= N; j++) {
+        c[0][j] = 0;
+    }
+
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            if (X[i-1] == Y[j-1]) {
+                c[i][j] = c[i-1][j-1] + 1;
+                b[i][j] = 1;
+            } else if (c[i-1][j] >= c[i][j-1]) {
+                c[i][j] = c[i-1][j];
+                b[i][j] = 2;
+            } else {
+                c[i][j] = c[i][j-1];
+                b[i][j] = 3;
+            }
+        }
+    }
+
+    seq = new int[c[M][N]];
+    build_sequence(c[M][N] - 1, M, N);
+}
+
+int length() {
+    return c[M][N];
+}
+
+int element(int i) {
+    return seq[i];
+}
+
+
