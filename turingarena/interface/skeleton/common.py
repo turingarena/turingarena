3080867e9d60900ae1fb57a43d048f3752197a98
Massimo Cairo <cairomassimo@gmail.com>
2018-03-25 18:32:35 +0200

    Use a class for C++ code generation.

--- turingarena/interface/skeleton/common.py
+++ turingarena/interface/skeleton/common.py
@@ -8,3 +8,86 @@ class ExpressionBuilder:
     def reference(self, e):
         subscripts = "".join(f"[{self.build(index)}]" for index in e.indices)
         return f"{e.variable_name}{subscripts}"
+
+
+class CodeGen:
+    def block_content(self, b):
+        for s in b.statements:
+            yield from self.statement(s)
+
+    def statement(self, s):
+        method_name = f"{s.statement_type}_statement"
+        try:
+            return getattr(self, method_name)(s)
+        except NotImplementedError:
+            return self.any_statement(s)
+
+    def var_statement(self, s):
+        raise NotImplementedError
+
+    def function_statement(self, s):
+        raise NotImplementedError
+
+    def callback_statement(self, s):
+        raise NotImplementedError
+
+    def init_statement(self, s):
+        raise NotImplementedError
+
+    def main_statement(self, s):
+        raise NotImplementedError
+
+    def input_statement(self, s):
+        raise NotImplementedError
+
+    def output_statement(self, s):
+        raise NotImplementedError
+
+    def checkpoint_statement(self, s):
+        raise NotImplementedError
+
+    def flush_statement(self, s):
+        raise NotImplementedError
+
+    def break_statement(self, s):
+        raise NotImplementedError
+
+    def continue_statement(self, s):
+        raise NotImplementedError
+
+    def exit_statement(self, s):
+        raise NotImplementedError
+
+    def alloc_statement(self, s):
+        raise NotImplementedError
+
+    def return_statement(self, s):
+        raise NotImplementedError
+
+    def call_statement(self, s):
+        raise NotImplementedError
+
+    def if_statement(self, s):
+        raise NotImplementedError
+
+    def switch_statement(self, s):
+        raise NotImplementedError
+
+    def for_statement(self, s):
+        raise NotImplementedError
+
+    def loop_statement(self, s):
+        raise NotImplementedError
+
+    def any_statement(self, s):
+        raise NotImplementedError
+
+    def expression(self, e):
+        return getattr(self, f"{e.expression_type}_expression")(e)
+
+    def int_literal_expression(self, e):
+        return f"{e.value}"
+
+    def reference_expression(self, e):
+        subscripts = "".join(f"[{self.expression(index)}]" for index in e.indices)
+        return f"{e.variable_name}{subscripts}"
--- turingarena/interface/skeleton/cpp.py
+++ turingarena/interface/skeleton/cpp.py
@@ -1,152 +1,131 @@
 from turingarena.common import indent_all, indent
-from turingarena.interface.skeleton.common import ExpressionBuilder
+from turingarena.interface.context import StaticGlobalContext
+from turingarena.interface.skeleton.common import CodeGen
 
 
 def generate_skeleton_cpp(interface):
     yield "#include <cstdio>"
     yield "#include <cstdlib>"
-    for statement in interface.statements:
-        yield
-        yield from generate_skeleton_statement(statement)
+    yield from SkeletonCodeGen().block_content(interface)
     yield
     yield from generate_main(interface)
 
 
-def generate_template_cpp(interface):
-    for statement in interface.statements:
-        yield
-        yield from generate_template_statement(statement)
-
-
-def generate_skeleton_statement(statement):
-    generators = {
-        "var": lambda: ["extern " + build_declaration(statement)],
-        "function": lambda: generate_function(statement),
-        "callback": lambda: generate_callback(statement),
-        "init": lambda: [],
-        "main": lambda: [],
-    }
-    return generators[statement.statement_type]()
-
-
-def generate_template_statement(statement):
-    generators = {
-        "var": lambda: [build_declaration(statement)],
-        "function": lambda: generate_function_template(statement),
-        "callback": lambda: generate_callback_template(statement),
-        "init": lambda: [],
-        "main": lambda: [],
-    }
-    return generators[statement.statement_type]()
-
-
-def generate_function(statement):
-    yield f"{build_callable_declarator(statement.function)};"
-
-
-def generate_function_template(statement):
-    yield f"{build_callable_declarator(statement.function)}" " {"
-    yield indent("// TODO")
-    yield "}"
-
-
-def generate_callback(statement):
-    callback = statement.callback
-    yield f"{build_callable_declarator(callback)}" " {"
-    yield indent(rf"""printf("%s\n", "{callback.name}");""")
-    yield from indent_all(generate_block(callback.body))
-    yield "}"
-
-
-def generate_callback_template(statement):
-    callback = statement.callback
-    yield f"{build_callable_declarator(callback)};"
-
-
 def generate_main(interface):
     yield "int main() {"
-    for statement in interface.statements:
-        if statement.statement_type in ("init", "main"):
-            yield from indent_all(generate_block(statement.body))
+    for s in interface.statements:
+        if s.statement_type in ("init", "main"):
+            yield from indent_all(SkeletonCodeGen().block_content(s.body))
     yield "}"
 
 
-def generate_block(block):
-    for statement in block.statements:
-        yield from generate_block_statement(statement)
+def generate_template_cpp(interface):
+    yield from TemplateCodeGen().block_content(interface)
 
 
-def generate_block_statement(statement):
-    generators = {
-        "var": lambda: [build_declaration(statement)],
-        "alloc": lambda: generate_alloc(statement),
-        "input": lambda: generate_input(statement),
-        "output": lambda: generate_output(statement),
-        "checkpoint": lambda: [r"""printf("%d\n", 0);"""],
-        "flush": lambda: ["fflush(stdout);"],
-        "call": lambda: generate_call(statement),
-        "for": lambda: generate_for(statement),
-        "if": lambda: generate_if(statement),
-        "exit": lambda: ["exit(0);"],
-        "return": lambda: [f"return {build_expression(statement.value)};"],
-    }
-    return generators[statement.statement_type]()
+class CppCodeGen(CodeGen):
+    pass
+
+
+class SkeletonCodeGen(CppCodeGen):
+    def callback_statement(self, s):
+        callback = s.callback
+        yield f"{build_callable_declarator(callback)}" " {"
+        yield indent(rf"""printf("%s\n", "{callback.name}");""")
+        yield from indent_all(self.block_content(callback.body))
+        yield "}"
+
+    def function_statement(self, s):
+        yield f"{build_callable_declarator(s.function)};"
+
+    def var_statement(self, s):
+        if isinstance(s.context, StaticGlobalContext):
+            yield "extern " + build_declaration(s)
+        else:
+            yield build_declaration(s)
+
+    def alloc_statement(self, s):
+        for argument in s.arguments:
+            arg = self.expression(argument)
+            value_type = build_full_type(argument.value_type.item_type)
+            size = self.expression(s.size)
+            yield f"{arg} = new {value_type}[{size}];"
+
+    def call_statement(self, s):
+        function_name = s.function.name
+        parameters = ", ".join(self.expression(p) for p in s.parameters)
+        if s.return_value is not None:
+            return_value = self.expression(s.return_value)
+            yield f"{return_value} = {function_name}({parameters});"
+        else:
+            yield f"{function_name}({parameters});"
+        if s.context.global_context.callbacks:
+            yield r"""printf("return\n");"""
+
+    def output_statement(self, s):
+        format_string = ' '.join("%d" for _ in s.arguments) + r'\n'
+        args = ', '.join(self.expression(v) for v in s.arguments)
+        yield f'printf("{format_string}", {args});'
+
+    def input_statement(self, statement):
+        format_string = ''.join("%d" for _ in statement.arguments)
+        args = ', '.join("&" + self.expression(v) for v in statement.arguments)
+        yield f'scanf("{format_string}", {args});'
+
+    def if_statement(self, s):
+        condition = self.expression(s.condition)
+        yield f"if( {condition} )" " {"
+        yield from indent_all(self.block_content(s.then_body))
+        yield "}"
+        if s.else_body is not None:
+            yield "else {"
+            yield from indent_all(self.block_content(s.else_body))
+            yield "}"
+
+    def for_statement(self, s):
+        index_name = s.index.variable.name
+        size = self.expression(s.index.range)
+        yield f"for(int {index_name} = 0; {index_name} < {size}; {index_name}++)" " {"
+        yield from indent_all(self.block_content(s.body))
+        yield "}"
+
+    def any_statement(self, s):
+        generators = {
+            "checkpoint": lambda: [r"""printf("%d\n", 0);"""],
+            "flush": lambda: ["fflush(stdout);"],
+            "exit": lambda: ["exit(0);"],
+            "return": lambda: [f"return {self.expression(s.value)};"],
+            "main": lambda: [],
+            "init": lambda: [],
+        }
+        return generators[s.statement_type]()
+
+
+class TemplateCodeGen(CppCodeGen):
+    def var_statement(self, s):
+        yield build_declaration(s)
+
+    def function_statement(self, s):
+        yield f"{build_callable_declarator(s.function)}" " {"
+        yield indent("// TODO")
+        yield "}"
+
+    def callback_statement(self, s):
+        callback = s.callback
+        yield f"{build_callable_declarator(callback)};"
+
+    def any_statement(self, s):
+        generators = {
+            "var": lambda: ["extern " + build_declaration(s)],
+            "init": lambda: [],
+            "main": lambda: [],
+        }
+        return generators[s.statement_type]()
 
 
 def generate_declarators(declaration):
     for variable in declaration.variables:
         yield build_declarator(declaration.value_type, variable.name)
-
-
-def generate_alloc(statement):
-    for argument in statement.arguments:
-        arg = build_expression(argument)
-        value_type = build_full_type(argument.value_type.item_type)
-        size = build_expression(statement.size)
-        yield f"{arg} = new {value_type}[{size}];"
-
-
-def generate_call(statement):
-    function_name = statement.function.name
-    parameters = ", ".join(build_expression(p) for p in statement.parameters)
-    if statement.return_value is not None:
-        return_value = build_expression(statement.return_value)
-        yield f"{return_value} = {function_name}({parameters});"
-    else:
-        yield f"{function_name}({parameters});"
-    if statement.context.global_context.callbacks:
-        yield r"""printf("return\n");"""
-
-
-def generate_output(statement):
-    format_string = ' '.join("%d" for _ in statement.arguments) + r'\n'
-    args = ', '.join(build_expression(v) for v in statement.arguments)
-    yield f'printf("{format_string}", {args});'
-
-
-def generate_input(statement):
-    format_string = ''.join("%d" for _ in statement.arguments)
-    args = ', '.join("&" + build_expression(v) for v in statement.arguments)
-    yield f'scanf("{format_string}", {args});'
-
-
-def generate_if(statement):
-    condition = build_expression(statement.condition)
-    yield f"if( {condition} )" " {"
-    yield from indent_all(generate_block(statement.then_body))
-    yield "}"
-    if statement.else_body is not None:
-        yield "else {"
-        yield from indent_all(generate_block(statement.else_body))
-        yield "}"
-
-
-def generate_for(statement):
-    index_name = statement.index.variable.name
-    size = build_expression(statement.index.range)
-    yield f"for(int {index_name} = 0; {index_name} < {size}; {index_name}++)" " {"
-    yield from indent_all(generate_block(statement.body))
-    yield "}"
 
 
 def build_callable_declarator(callable):
@@ -164,10 +143,6 @@ def build_declaration(statement):
 def build_parameter(parameter):
     full_type = build_full_type(parameter.value_type)
     return f'{full_type} {parameter.name}'
-
-
-def build_expression(expression):
-    return ExpressionBuilder().build(expression)
 
 
 def build_declarator(value_type, name):
