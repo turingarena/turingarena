913410d8a2be0cab6dee4f625eaaf67de6675070
Giulio Mazzi <mazzi.giulio2@gmail.com>
2018-03-29 14:41:50 +0200

    WIP tic tac toe problem

---
+++ examples/tic_tac_toe/evaluate.py
@@ -0,0 +1,44 @@
+import random
+
+NUMBER_OF_MATCH = 100
+
+def pos_to_yx(pos):
+    return pos//3, pos%3
+
+def yx_to_pos(y,x):
+    return y*3 + x
+
+def evaluate(algorithm):
+
+    print("running game")
+
+    with algorithm.run(global_variables=dict(
+            position_mapping = [ [0,1,2], [3,4,5], [6,7,8] ]
+    )) as p:
+        memory_usage = p.sandbox.get_info().memory_usage
+        print(f"Memory usage: {memory_usage} bytes")
+
+        number_of_match = NUMBER_OF_MATCH
+        result = p.call.play_first_round()
+        print(result, pos_to_yx(result))
+
+#        number_of_match = None
+#        number_of_match = NUMBER_OF_MATCH
+#
+#        for i in range(number_of_match):
+#
+#            first_turn_for_player = result = continue_match = None
+#
+#            p.call.start_new_game()
+#
+#            first_turn_for_player = random.randint(0,1)
+#
+#            if first_turn_for_player:
+#                result = p.call.play_first_round()
+#
+#            continue_match = True
+#            for i in range(4):
+#                if continue_match:
+#                    result = p.call.play_a_round(
+#                            random.randint(0,2), random.randint(0,2))
+#                    continue_match = True
