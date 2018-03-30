7794d7e46bf0e21426b5570e2fc8e6068ba9cd54
Massimo Cairo <cairomassimo@gmail.com>
2018-03-30 16:29:17 +0200

    Change directory installation mechanism.

--- turingarena/cli/main.py
+++ turingarena/cli/main.py
@@ -1,9 +1,10 @@
 from turingarena.cli import docopt_cli
 from turingarena.cli.loggerinit import init_logger
+from turingarena.config import install_cli, uninstall_cli
 from turingarena.container.cli import container_cli
 from turingarena.interface.cli import generate_metadata_cli, generate_template_cli, generate_skeleton_cli
 from turingarena.problem.cli import evaluate_cli
-from turingarena.siteinstall import install_cli, uninstall_cli
+from turingarena.pythonsite import configure_python_site_cli
 from turingarena.tests.cli import test_cli
 from turingarena.web.serve import serve_cli
 
@@ -14,9 +15,11 @@ def main(args):
 
     Usage:
       turingarena [options] <cmd> [<args>]...
+      turingarena --help-commands
 
     Options:
       --log-level=<level>  Set logging level.
+      --help-commands  Show list of available commands.
     """
     init_logger(args)
 
@@ -30,7 +33,19 @@ def main(args):
         "test": test_cli,
         "install": install_cli,
         "uninstall": uninstall_cli,
+        "pythonsite": configure_python_site_cli,
     }
+
+    if args["--help-commands"]:
+        print("Avaliable commands:")
+        print()
+        for c, cli in commands.items():
+            doc = cli.__doc__.splitlines(keepends=False)[0]
+            print(f"{c:20} {doc}")
+        print()
+        print("For further help: turingarena <command> -h")
+        return
+
     argv2 = args["<args>"]
     commands[args["<cmd>"]](argv2)
 
--- turingarena/tests/cli.py
+++ turingarena/tests/cli.py
@@ -1,5 +1,15 @@
 import pytest
 
+from turingarena.cli import docopt_cli
 
+
+@docopt_cli
 def test_cli(args):
-    return pytest.main(args)
+    """
+    Usage:
+        test <pytestargs>...
+
+    Options:
+        <pytestargs>  Options to pass to pytest
+    """
+    return pytest.main(args["<pytestargs>"])
