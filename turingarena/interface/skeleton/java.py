f5e7e31dfbe173f9b45e28fc0a5ec027e5ae6edf
alerighi <alessandro.righi@outlook.it>
2018-03-30 13:30:16 +0200

    Restructure java skeleton generation to use new classes

--- turingarena/interface/skeleton/java.py
+++ turingarena/interface/skeleton/java.py
@@ -1,166 +1,127 @@
 from turingarena.common import indent_all, indent
-from turingarena.interface.skeleton.common import ExpressionBuilder
+from turingarena.interface.skeleton.common import ExpressionBuilder, CodeGen
+from turingarena.interface.context import StaticGlobalContext
 
 
 def generate_skeleton_java(interface):
-    yield "import java.util.Scanner;"
-    yield
-    yield "abstract class Skeleton {"
-    yield indent("private static final Scanner in = new Scanner(System.in);")
-    for statement in interface.body.statements:
-        yield
-        yield from indent_all(generate_skeleton_statement(statement, interface=interface))
-    yield "}"
-    yield
+    yield from JavaSkeletonCodeGen(interface).generate()
 
 
 def generate_template_java(interface):
-    yield "class Solution extends Skeleton {"
-    for statement in interface.body.statements:
-        yield from indent_all(generate_template_statement(statement, interface=interface))
-    yield "}"
+    yield from JavaTemplateCodeGen(interface).generate()
 
 
-def generate_skeleton_statement(statement, *, interface):
-    generators = {
-        "var": lambda: ["static final " + build_declaration(statement)],
-        "function": lambda: generate_function(statement),
-        "callback": lambda: generate_callback(statement, interface=interface),
-        "main": lambda: generate_main(statement, interface=interface),
-        "init": lambda: generate_constructor(statement, interface=interface),
-    }
-    return generators[statement.statement_type]()
+class JavaSkeletonCodeGen(CodeGen):
+    def generate(self):
+        yield "import java.util.Scanner;"
+        yield
+        yield "abstract class Skeleton {"
+        yield indent("private static final Scanner in = new Scanner(System.in);")
+        yield
+        yield from indent_all(self.block_content(self.interface.body))
+        yield "}"
+        yield
+
+    def function_statement(self, statement):
+        yield f"abstract {build_callable_declarator(statement.function)};"
+
+    def callback_statement(self, statement):
+        callback = statement.callback
+        yield f"{build_callable_declarator(callback)}" " {"
+        yield indent(f'System.out.println("{callback.name}");')
+        yield from indent_all(self.block_content(statement.callback.body))
+        yield "}"
+        yield
+
+    def main_statement(self, statement):
+        yield
+        yield "public static void main(String args[]) {"
+        yield indent("Solution solution = new Solution();")
+        yield from indent_all(self.block_content(statement.body))
+        yield "}"
+
+    def init_statement(self, statement):
+        yield
+        yield "static {"
+        yield from indent_all(self.block_content(statement.body))
+        yield "}"
+
+    def alloc_statement(self, statement):
+        size = build_expression(statement.size)
+        for argument in statement.arguments:
+            arg = build_expression(argument)
+            yield f"{arg} = new {build_alloc_type(argument.value_type.item_type, size)};"
+
+    def call_statement(self, statement):
+        function_name = statement.function_name
+        parameters = ", ".join(build_expression(p) for p in statement.parameters)
+        if statement.return_value is not None:
+            return_value = build_expression(statement.return_value)
+            yield f"{return_value} = solution.{function_name}({parameters});"
+        else:
+            yield f"solution.{function_name}({parameters});"
+        if statement.context.global_context.callbacks:
+            yield 'System.out.println("return");'
+
+    def output_statement(self, statement):
+        format_string = ' '.join("%d" for _ in statement.arguments) + r'\n'
+        args = ', '.join(build_expression(v) for v in statement.arguments)
+        yield f'System.out.printf("{format_string}", {args});'
+
+    def input_statement(self, statement):
+        for arg in statement.arguments:
+            yield f"{build_expression(arg)} = " + {
+                int: "in.nextInt()",
+            }[arg.value_type.base_type] + ";"
+
+    def if_statement(self, statement):
+        condition = build_expression(statement.condition)
+        yield f"if ({condition})" " {"
+        yield from indent_all(self.block_content(statement.body))
+        if statement.else_body is not None:
+            yield "} else {"
+            yield from indent_all(self.block_content(statement.body))
+        yield "}"
+
+    def for_statement(self, statement):
+        index_name = statement.index.variable.name
+        size = build_expression(statement.index.range)
+        yield f"for (int {index_name} = 0; {index_name} < {size}; {index_name}++)" " {"
+        yield from indent_all(self.block_content(statement.body))
+        yield "}"
+
+    def var_statement(self, s):
+        if isinstance(s.context, StaticGlobalContext):
+            yield "static final " + build_declaration(s)
+        else:
+            yield build_declaration(s)
+
+    def any_statement(self, statement):
+        generators = {
+            "flush": lambda: ["System.out.flush();"],
+            "checkpoint": lambda: ["""System.out.println("0");"""],
+            "exit": lambda: ["System.exit(0);"],
+            "return": lambda: [f"return {build_expression(statement.value)};"],
+        }
+        return generators[statement.statement_type]()
 
 
-def generate_template_statement(statement, *, interface):
-    generators = {
-        "var": lambda: ["// global " + build_declaration(statement)],
-        "function": lambda: generate_function_template(statement),
-        "callback": lambda: [],
-        "main": lambda: [],
-        "init": lambda: [],
-    }
-    return generators[statement.statement_type]()
+class JavaTemplateCodeGen(CodeGen):
+    def generate(self):
+        yield "class Solution extends Skeleton {"
+        yield from indent_all(self.block_content(self.interface.body))
+        yield "}"
 
+    def function_statement(self, statement):
+        yield
+        yield f"{build_callable_declarator(statement.function)}" " {"
+        yield indent("// TODO")
+        if statement.function.return_type:
+            yield indent("return 0;")
+        yield "}"
 
-def generate_function(statement):
-    yield f"abstract {build_callable_declarator(statement.function)};"
-
-
-def generate_function_template(statement):
-    yield
-    yield f"{build_callable_declarator(statement.function)}" " {"
-    yield indent("// TODO")
-    if statement.function.return_type:
-        yield indent("return 0;")
-    yield "}"
-
-
-def generate_callback(statement, *, interface):
-    callback = statement.callback
-    yield f"{build_callable_declarator(callback)}" " {"
-    yield indent(rf"""System.out.println("{callback.name}");""")
-    yield from indent_all(generate_block(callback.body, interface=interface))
-    yield "}"
-
-
-def generate_callback_template(statement, *, interface):
-    callback = statement.callback
-    yield f"{build_callable_declarator(callback)};"
-
-
-def generate_main(statement, *, interface):
-    yield "public static void main(String args[]) {"
-    yield indent("Solution solution = new Solution();")
-    yield from indent_all(generate_block(statement.body, interface=interface))
-    yield "}"
-
-
-def generate_constructor(statement, *, interface):
-    yield "static {"
-    yield from indent_all(generate_block(statement.body, interface=interface))
-    yield "}"
-
-
-def generate_block(block, *, interface):
-    for statement in block.statements:
-        yield from generate_block_statement(statement, interface=interface)
-
-
-def generate_block_statement(statement, *, interface):
-    generators = {
-        "var": lambda: [build_declaration(statement)],
-        "alloc": lambda: generate_alloc(statement),
-        "input": lambda: generate_input(statement),
-        "output": lambda: generate_output(statement),
-        "flush": lambda: ["System.out.flush();"],
-        "checkpoint": lambda: ["""System.out.println("0");"""],
-        "call": lambda: generate_call(statement, interface=interface, ),
-        "for": lambda: generate_for(statement, interface=interface),
-        "if": lambda: generate_if(statement, interface=interface),
-        "exit": lambda: ["System.exit(0);"],
-        "return": lambda: [f"return {build_expression(statement.value)};"],
-    }
-    return generators[statement.statement_type]()
-
-
-def generate_alloc(statement):
-    size = build_expression(statement.size)
-    for argument in statement.arguments:
-        arg = build_expression(argument)
-        yield f"{arg} = new {build_alloc_type(argument.value_type.item_type, size)};"
-
-
-def build_alloc_type(var_type, size):
-    if var_type.meta_type == "array":
-        return build_alloc_type(var_type.item_type, size) + "[]"
-    else:
-        return {
-                   int: "int",
-               }[var_type.base_type] + f"[{size}]"
-
-
-def generate_call(statement, *, interface):
-    function_name = statement.function_name
-    parameters = ", ".join(build_expression(p) for p in statement.parameters)
-    if statement.return_value is not None:
-        return_value = build_expression(statement.return_value)
-        yield f"{return_value} = solution.{function_name}({parameters});"
-    else:
-        yield f"solution.{function_name}({parameters});"
-    if interface.callbacks:
-        yield 'System.out.println("return");'
-
-
-def generate_output(statement):
-    format_string = ' '.join("%d" for _ in statement.arguments) + r'\n'
-    args = ', '.join(build_expression(v) for v in statement.arguments)
-    yield f'System.out.printf("{format_string}", {args});'
-
-
-def generate_input(statement):
-    for arg in statement.arguments:
-        yield f"{build_expression(arg)} = " + {
-            int: "in.nextInt()",
-        }[arg.value_type.base_type] + ";"
-
-
-def generate_if(statement, *, interface):
-    condition = build_expression(statement.condition)
-    yield f"if ({condition})" " {"
-    yield from indent_all(generate_block(statement.then_body, interface=interface))
-    if statement.else_body is not None:
-        yield "} else {"
-        yield from indent_all(generate_block(statement.else_body, interface=interface))
-    yield "}"
-
-
-def generate_for(statement, *, interface):
-    index_name = statement.index.variable.name
-    size = build_expression(statement.index.range)
-    yield f"for (int {index_name} = 0; {index_name} < {size}; {index_name}++)" " {"
-    yield from indent_all(generate_block(statement.body, interface=interface))
-    yield "}"
+    def any_statement(self, s):
+        return []
 
 
 def build_callable_declarator(callable):
@@ -194,3 +155,13 @@ def build_type(value_type):
         "array": lambda: f"{build_type(value_type.item_type)}[]"
     }
     return builders[value_type.meta_type]()
+
+
+def build_alloc_type(var_type, size):
+    if var_type.meta_type == "array":
+        return build_alloc_type(var_type.item_type, size) + "[]"
+    else:
+        return {
+            int: "int",
+        }[var_type.base_type] + f"[{size}]"
+
