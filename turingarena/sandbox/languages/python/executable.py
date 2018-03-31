e237483f2abbf84b4003de67efa4ae364fe18f6d
alerighi <alessandro.righi@outlook.it>
2018-03-31 19:49:41 +0200

    Refactor PythonAlgorithmExecutableScript

--- turingarena/sandbox/languages/python/executable.py
+++ turingarena/sandbox/languages/python/executable.py
@@ -1,15 +1,11 @@
-import logging
-import os
 import shutil
 import subprocess
+import pkg_resources
+
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 
-import pkg_resources
-
 from turingarena.sandbox.executable import AlgorithmExecutable
-
-logger = logging.getLogger(__name__)
 
 
 class PythonAlgorithmExecutableScript(AlgorithmExecutable):
@@ -19,19 +15,16 @@ class PythonAlgorithmExecutableScript(AlgorithmExecutable):
     def run(self, connection):
         # get file path
         sandbox_path = pkg_resources.resource_filename(__name__, "sandbox.py")
-        source_path = os.path.join(self.algorithm_dir, "source.py")
-        skeleton_path = os.path.join(self.algorithm_dir, "skeleton.py")
 
         # create tmp directory
         with TemporaryDirectory(dir="/tmp", prefix="python_cwd_") as cwd:
             # copy files into tmp dir
-            shutil.copyfile(sandbox_path, os.path.join(cwd, "sandbox.py"))
-            shutil.copy(source_path, cwd)
-            shutil.copy(skeleton_path, cwd)
+            shutil.copy(f"{self.algorithm_dir}/source.py", cwd)
+            shutil.copy(f"{self.algorithm_dir}/skeleton.py", cwd)
 
             # run process
             popen = subprocess.Popen(
-                ["python", "sandbox.py"],
+                ["python", sandbox_path],
                 universal_newlines=True,
                 # preexec_fn=lambda: set_memory_and_time_limits(),
                 cwd=cwd,
