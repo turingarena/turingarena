ef674940dca7fd2e54175eab12795402b9dc4ed4
alerighi <alessandro.righi@outlook.it>
2018-03-31 19:52:08 +0200

    Refactor JavaScriptExecutableScript

--- turingarena/sandbox/languages/javascript/executable.py
+++ turingarena/sandbox/languages/javascript/executable.py
@@ -1,16 +1,12 @@
-import logging
-import os
 import shutil
 import subprocess
+import pkg_resources
+
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 
-import pkg_resources
-
 from turingarena.sandbox.executable import AlgorithmExecutable
 from turingarena.sandbox.rlimits import set_memory_and_time_limits
-
-logger = logging.getLogger(__name__)
 
 
 class JavaScriptAlgorithmExecutableScript(AlgorithmExecutable):
@@ -18,20 +14,14 @@ class JavaScriptAlgorithmExecutableScript(AlgorithmExecutable):
 
     @contextmanager
     def run(self, connection):
-        # get file path
         sandbox_path = pkg_resources.resource_filename(__name__, "sandbox.js")
-        source_path = os.path.join(self.algorithm_dir, "source.js")
-        skeleton_path = os.path.join(self.algorithm_dir, "skeleton.js")
 
-        # create tmp directory
         with TemporaryDirectory(dir="/tmp", prefix="javascript_cwd_") as cwd:
-            # copy files into tmp dir
-            shutil.copy(sandbox_path, cwd)
-            shutil.copy(source_path, cwd)
-            shutil.copy(skeleton_path, cwd)
+            shutil.copy(f"{self.algorithm_dir}/source.js", cwd)
+            shutil.copy(f"{self.algorithm_dir}/skeleton.js", cwd)
 
-            popen = subprocess.Popen(
-                ["node", "sandbox.js"],
+            process = subprocess.Popen(
+                ["node", sandbox_path],
                 universal_newlines=True,
                 preexec_fn=lambda : set_memory_and_time_limits(memory_limit=None, time_limit=5),
                 cwd=cwd,
@@ -39,6 +29,6 @@ class JavaScriptAlgorithmExecutableScript(AlgorithmExecutable):
                 stdout=connection.upward,
                 bufsize=1,
             )
-            # run process
-            with self.manage_process(popen) as p:
+
+            with self.manage_process(process) as p:
                 yield p
