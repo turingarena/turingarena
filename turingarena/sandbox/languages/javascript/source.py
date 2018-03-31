c5773044d14b95d423f9774ccb16070c9bb7a530
alerighi <alessandro.righi@outlook.it>
2018-03-31 19:59:11 +0200

    Removed unused logger

--- turingarena/sandbox/languages/javascript/source.py
+++ turingarena/sandbox/languages/javascript/source.py
@@ -1,10 +1,5 @@
-import logging
-
 from turingarena.sandbox.source import AlgorithmSource
-
-logger = logging.getLogger(__name__)
 
 
 class JavascriptAlgorithmSource(AlgorithmSource):
     __slots__ = []
-
--- turingarena/sandbox/languages/python/source.py
+++ turingarena/sandbox/languages/python/source.py
@@ -1,8 +1,4 @@
-import logging
-
 from turingarena.sandbox.source import AlgorithmSource
-
-logger = logging.getLogger(__name__)
 
 
 class PythonAlgorithmSource(AlgorithmSource):
