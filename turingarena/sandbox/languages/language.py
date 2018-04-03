3ec804ae3fb6f99a167a921e3fdfad5da277186c
alerighi <alessandro.righi@outlook.it>
2018-04-03 22:42:26 +0200

    Use reflection to find languages

--- turingarena/sandbox/languages/language.py
+++ turingarena/sandbox/languages/language.py
@@ -1,3 +1,6 @@
+import pkgutil
+import importlib
+
 from collections import namedtuple
 
 
@@ -13,24 +16,35 @@ class Language(namedtuple("Language", [
 
     def __new__(cls, *args, **kwargs):
         if args:
+            if args[0][0] == ".":
+                return cls.from_extension(args[0][1:])
             return cls.from_name(args[0])
         return super().__new__(cls, *args, **kwargs)
 
     @staticmethod
-    def from_name(name):
-        from .cpp import language as cpp
-        from .java import language as java
-        from .javascript import language as javascript
-        from .python import language as python
+    def languages():
+        import turingarena.sandbox.languages as language_mod
 
-        languages = {
-            "c++": cpp,
-            "java": java,
-            "javascript": javascript,
-            "python": python,
-        }
+        languages = []
+        for mod in pkgutil.iter_modules(language_mod.__path__):
+            if mod.ispkg:
+                mod = importlib.import_module(f"turingarena.sandbox.languages.{mod.name}")
+                try:
+                    languages.append(getattr(mod, "language"))
+                except AttributeError:
+                    pass
+        return languages
 
-        try:
-            return languages[name]
-        except KeyError:
-            raise RuntimeError(f"Language {name} not supported by TuringArena")
+    @classmethod
+    def from_name(cls, name):
+        for language in cls.languages():
+            if language.name == name:
+                return language
+        raise RuntimeError(f"language {name} not supported by TuringArena")
+
+    @classmethod
+    def from_extension(cls, extension):
+        for language in cls.languages():
+            if language.extension == extension:
+                return language
+        raise RuntimeError(f"no language with extension{extension} is supported by TuringArena")
