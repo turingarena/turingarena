f26bca05b3d97ac03017b1b81d6e892ad7515dfa
Massimo Cairo <cairomassimo@gmail.com>
2018-04-25 10:35:05 +0200

    pytestplugin: make a separate item for each evaluation assert
    Closes #14.

--- examples/sum_of_two_numbers/solutions/always_wrong.py
+++ examples/sum_of_two_numbers/solutions/always_wrong.py
@@ -1,3 +1,4 @@
 # evaluation_assert not data["goals"]["correct"]
+# evaluation_assert all("wrong" in line for line in stdout)
 def sum(a, b):
     return a + b + 1
--- turingarena/pytestplugin.py
+++ turingarena/pytestplugin.py
@@ -2,6 +2,7 @@ import ast
 import json
 import os
 import re
+from functools import lru_cache
 
 import pytest
 from _pytest.assertion.rewrite import rewrite_asserts
@@ -15,40 +16,32 @@ class EvaluationAssertionError(Exception):
     pass
 
 
-class ProblemSolutionTestItem(pytest.Item):
-    def __init__(self, parent, evaluator_name, source_name):
-        super().__init__("test_solution", parent)
-        self.evaluator_name = evaluator_name
-        self.source_name = source_name
+class ProblemSolutionEvaluationItem(pytest.Item):
+    def __init__(self, parent):
+        super().__init__("evaluation", parent)
 
     def runtest(self):
-        assertions = self.load_assertion_in_source()
+        self.parent.evaluate(self)
 
-        result = HostPythonEvaluator(
-            self.evaluator_name,
-            interface_name=self.evaluator_name,
-        ).evaluate(self.source_name)
 
-        self.add_report_section("call", "evaluation_stdout", "\n".join(result.stdout))
-        self.add_report_section("call", "evaluation_result", json.dumps(result.data, indent=4))
+class ProblemSolutionAssertionItem(pytest.Item):
+    def __init__(self, parent, assertion):
+        super().__init__(f"evaluation_assert {assertion}", parent)
+        self.assertion = assertion
 
-        for condition in assertions:
-            mode = "exec"
-            tree = ast.parse(f"assert {condition}\n", mode=mode)
-            rewrite_asserts(tree)
-            co = compile(tree, filename="<evaluation_assert>", mode=mode, dont_inherit=True)
-            try:
-                exec(co, dict(approx=approx), result._asdict())
-            except AssertionError as e:
-                raise EvaluationAssertionError(condition) from e
-            except Exception as e:
-                raise AssertionError(f"exception while checking: {condition}") from e
+    def runtest(self):
+        result = self.parent.evaluate(self)
 
-    def load_assertion_in_source(self):
-        mod, rel_path = split_module(self.source_name, default_arg="interface.txt")
-        with open(find_package_path(mod, rel_path)) as f:
-            source_text = f.read()
-        return re.findall(r"evaluation_assert\s+(.+)", source_text)
+        mode = "exec"
+        tree = ast.parse(f"assert {self.assertion}\n", mode=mode)
+        rewrite_asserts(tree)
+        co = compile(tree, filename="<evaluation_assert>", mode=mode, dont_inherit=True)
+        try:
+            exec(co, dict(approx=approx), result._asdict())
+        except AssertionError as e:
+            raise EvaluationAssertionError(self.assertion) from e
+        except Exception as e:
+            raise AssertionError(f"exception while checking: {self.assertion}") from e
 
     def repr_failure(self, excinfo):
         if isinstance(excinfo.value, EvaluationAssertionError):
@@ -67,8 +60,29 @@ class ProblemSolutionTestFile(pytest.File):
         self.evaluator_name = evaluator_name
         self.source_name = source_name
 
+    @lru_cache(None)
+    def _do_evaluate(self):
+        return HostPythonEvaluator(
+            self.evaluator_name,
+            interface_name=self.evaluator_name,
+        ).evaluate(self.source_name)
+
+    def evaluate(self, item):
+        result = self._do_evaluate()
+        item.add_report_section("call", "evaluation_stdout", "\n".join(result.stdout))
+        item.add_report_section("call", "evaluation_result", json.dumps(result.data, indent=4))
+        return result
+
+    def load_assertion_in_source(self):
+        mod, rel_path = split_module(self.source_name, default_arg="interface.txt")
+        with open(find_package_path(mod, rel_path)) as f:
+            source_text = f.read()
+        return re.findall(r"evaluation_assert\s+(.+)", source_text)
+
     def collect(self):
-        yield ProblemSolutionTestItem(self, self.evaluator_name, self.source_name)
+        yield ProblemSolutionEvaluationItem(self)
+        for assertion in self.load_assertion_in_source():
+            yield ProblemSolutionAssertionItem(self, assertion)
 
 
 def pytest_collect_file(path, parent):
