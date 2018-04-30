de2740cbea28c1de27fbdeb3bd2c178c56ad41b7
Massimo Cairo <cairomassimo@gmail.com>
2018-04-30 13:51:16 +0200

    Improve CLI server/client communication.
    - drop use of socat, use a port on localhost instead
    - do not run command as root on the server
    - create local Git DB if needed
    - improve console output

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 ARG BASE_IMAGE=turingarena/turingarena-base
 FROM $BASE_IMAGE
 
-COPY . /usr/local/turingarena/
 ENV TURINGARENA_PATH_FILE=/usr/lib/python3.6/site-packages/turingarena.pth
+
+COPY . /usr/local/turingarena/
+
 RUN true \
     && ssh-keygen -A \
+    && echo 'turingarena:x:1000:1000::/run/turingarena:/bin/ash' >> /etc/passwd \
+    && passwd -d turingarena \
     && ln -sf /usr/bin/python3 /usr/local/bin/python \
     && ln -s /usr/lib/jvm/default-jvm/bin/javac /usr/local/bin/javac \
     && ln -s /usr/lib/jvm/default-jvm/bin/jcmd /usr/local/bin/jcmd \
--- turingarena_cli.py
+++ turingarena_cli.py
@@ -10,36 +10,38 @@ def info(*args):
 
 
 def turingarena_cli():
-    socket_path = "/run/turingarena/sshd.sock"
     ssh_command = (
         [
             "ssh",
-            "-q",
-            "-o", "ProxyCommand=socat - UNIX-CONNECT:{socket_path}".format(socket_path=socket_path),
+            "-o", "BatchMode=yes",
+            "-o", "LogLevel=error",
             "-o", "UserKnownHostsFile=/dev/null",
             "-o", "StrictHostKeyChecking=no",
+            "-p", "20122",
         ]
     )
-    ssh_cli = ssh_command + ["-t", "root@localhost"]
 
     working_dir = subprocess.check_output(
         ["git", "rev-parse", "--show-toplevel"],
         universal_newlines=True,
     ).strip()
+
     current_dir = os.path.relpath(os.curdir, working_dir)
     info("Sending work dir: {working_dir} (current dir: {current_dir})...".format(
         working_dir=working_dir,
         current_dir=current_dir,
     ))
 
+    git_dir = os.path.join(os.path.expanduser("~"), ".turingarena", "db.git")
     git_env = {
         "GIT_WORK_TREE": working_dir,
-        "GIT_DIR": os.path.join(os.path.expanduser("~"), ".turingarena", "db.git"),
+        "GIT_DIR": git_dir,
         "GIT_SSH_COMMAND": " ".join("'" + c + "'" for c in ssh_command),
     }
 
     git_popen_args = dict(env=git_env, universal_newlines=True)
 
+    subprocess.check_call(["git", "init", "--quiet", "--bare", git_dir])
     subprocess.check_call(["git", "add", "-A", "."], **git_popen_args)
 
     tree_id = subprocess.check_output(["git", "write-tree"], **git_popen_args).strip()
@@ -50,24 +52,29 @@ def turingarena_cli():
         **git_popen_args
     ).strip()
 
-    subprocess.check_call(ssh_cli + ["mkdir -p /run/turingarena && git init --bare /run/turingarena/db.git -q"])
+    subprocess.check_call(ssh_command + [
+        "turingarena@localhost",
+        "git init --bare --quiet db.git",
+    ])
 
     subprocess.check_call([
         "git", "push", "-q",
-        "root@localhost:/run/turingarena/db.git",
+        "turingarena@localhost:db.git",
         "{commit_id}:refs/heads/sha-{commit_id}".format(commit_id=commit_id),
     ], **git_popen_args)
 
     # remove the remove branch (we only need the tree object)
     subprocess.check_call([
         "git", "push", "-q",
-        "root@localhost:/run/turingarena/db.git",
+        "turingarena@localhost:db.git",
         ":refs/heads/sha-{commit_id}".format(commit_id=commit_id),
     ], **git_popen_args)
 
     info("Work dir sent. Running command...")
 
-    subprocess.call(ssh_cli + [
+    subprocess.call(ssh_command + [
+        "-t",
+        "turingarena@localhost",
         "TURINGARENA_TREE_ID=" + tree_id,
         "TURINGARENA_CURRENT_DIR=" + current_dir,
         "/usr/local/bin/python", "-m", "turingarena_impl",
