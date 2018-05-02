9f40eb956fe95e1eef1656de2e0d8623cf800030
alerighi <alessandro.righi@outlook.it>
2018-05-02 15:16:48 +0200

    New grammar
    Change a ton of things in the grammar:
    - no more global variables and thus init block
    - functions are defined with the standard C syntax
    - functions now takes callback parameters in the form of C function pointers/lambdas
    - callbacks are now defined locally in blocks
    - remove var and alloc staments: variable declarations and allocation is now implicit
    - remove flush statement, that now is implicitely done before every read
    - change for syntax: now uses the form for <index> to <variable> {}
    - change syntax for switch: remove case label (switch <exp> { <litteral> { ... } [...] })
    - change syntax for function call: new syntax call <var> = <function>(<parameters>);
    - change syntax for if statement: do not require parentesis around condition

--- turingarena_impl/interface/grammar.py
+++ turingarena_impl/interface/grammar.py
@@ -3,132 +3,67 @@ grammar_ebnf = r"""
     @@eol_comments :: /\/\/.*$/
     @@left_recursion :: False
     
-    interface = statements:{ interface_statement }* $ ;
+    identifier = /[a-zA-Z_][0-9a-zA-Z_]*/;
+    string_literal = '"' @:/([^"\n]|\\")*/ '"';
+    int_literal = /0|-?[1-9][0-9]*/;
+
+    interface = function_declarations ~ main_block $;
     
-    interface_statement =
-        | var_statement
-        | statement_type:'function' ~ declarator:callable_declarator ';'
-        | statement_type:'callback' ~ declarator:callable_declarator body:block
-        | statement_type:'init' ~ body:block
-        | statement_type:'main' ~ body:block
-    ;
-
-    return_type_declarator =
-        '->' @:type_expression
-    ;
+    function_prototype = return_type:('int' | 'void') name:identifier '(' parameters:','.{ parameter_type }* ')';
+    function_declarations = block_name:`function_declarations` functions:{ function_declaration }*;
+    function_declaration = @:function_prototype ~ ';';
     
-    callable_declarator =
-        name:identifier '('
-            parameters:','.{ parameter_declaration }*
-        ')'
-        return_type:[ return_type_declarator ]
-    ;
-
-    var_statement =
-        statement_type:'var' ~ type:type_expression declarators:','.{ declarator }+ ';'
-    ;
+    main_block = block_name:'main' body:block;
     
-    declarator = name:identifier ;
+    parameter_type = 
+        | type:`callback` prototype:function_prototype
+        | type:`variable` 'int' name:identifier indexes:{'[' ']'}*
+        ;
 
-    index_declaration =
-        declarator:declarator ':' range:expression
-    ;
-
-    parameter_declaration = type:type_expression declarator:declarator ;
-
-    block = '{' statements:{ block_statement }* '}' ;
-
-    return_value_expression = '->' ~ @:expression ;
+    block = '{' statements:{ block_statement }* '}';
 
     block_statement =
-        | var_statement
-        | statement_type:('read'|'write') ~ arguments:expression_list ';'
-        | statement_type:('checkpoint'|'flush'|'break'|'continue'|'exit') ~ ';'
-        | statement_type:'alloc' ~ arguments:expression_list ':' size:expression ';'
+        | statement_type:('read' | 'write') ~ arguments:','.{ expression }* ';'
+        | statement_type:('checkpoint' | 'break' | 'continue' | 'exit') ~ ';'
         | statement_type:'return' ~ value:expression ';'
-        | statement_type:'call' ~ function_name:identifier
-            '(' parameters:expression_list ')'
-            return_value:[ return_value_expression ] ';'
-        | statement_type:'if' ~ condition:expression
-            then_body:block else_body:[ else_body ]
-        | statement_type:'switch' ~ value:expression '{' cases:{ switch_case }+ default:[default_case] '}'
-        | statement_type:'for' ~ '(' index:index_declaration ')' body:block
+        | statement_type:'if' ~ condition:expression then_body:block else_body:[ else_body ]
+        | statement_type:'switch' ~ value:expression '{' cases:{ switch_case }+ '}'
+        | statement_type:'for' ~ index:identifier 'to' range:identifier body:block
         | statement_type:'loop' ~ body:block
-    ;
+        | statement_type:'call' ~ [return_variable:identifier '='] name:identifier '(' parameters:','.{ expression }* ')' ';'
+        | statement_type:`callback` prototype:function_prototype ~ (body:block | 'default' ';')
+        ;
 
-    else_body = 'else' ~ @:block ;
-
-    expression_list = ','.{ expression }* ;
-
-    default_case = 
-        'default' body:block
-    ;
-
-    switch_case =
-        'case' labels:','.{expression}+ body:block
-    ;
+    else_body = 'else' ~ @:block;
+    switch_case = labels:','.{ int_literal }+ body:block;
     
-    expression = or_expression ;
+    expression = or_expression;
     or_expression = 
-        |
-            expression_type:`or`
-            operands:and_expression
-            { '||' operands:and_expression }+
+        | expression_type:`or` operands:and_expression { '||' operands:and_expression }+
         | and_expression
-    ;
+        ;
     and_expression =
-        |
-            expression_type:`and`
-            operands:comparison_expression
-            { '&&' operands:comparison_expression }+
+        | expression_type:`and` operands:comparison_expression { '&&' operands:comparison_expression }+
         | comparison_expression
-    ;
+        ;
     comparison_expression =
-        |
-            expression_type:`comparison`
-            operands:sum_expression
-            {
-                operators+:('=='|'!='|'<'|'<='|'>'|'>=')
-                operands:sum_expression
-            }+
+        | expression_type:`comparison` operands:sum_expression { operators+:('=='|'!='|'<'|'<='|'>'|'>=') operands:sum_expression }+
         | sum_expression
-    ;
+        ;
     sum_expression = 
-        |
-            expression_type:`sum`
-            signs:() operands:mul_expression
-            { signs:('+'|'-') operands:mul_expression }+
-        |
-            expression_type:`sum`
-            signs+:('+'|'-') operands:mul_expression
-            { signs+:('+'|'-') operands:mul_expression }*
+        | expression_type:`sum` signs:() operands:mul_expression { signs:('+'|'-') operands:mul_expression }+
+        | expression_type:`sum` signs+:('+'|'-') operands:mul_expression { signs+:('+'|'-') operands:mul_expression }*
         | mul_expression
-    ;
+        ;
     mul_expression =
-        |
-            expression_type:`mul`
-            operands:atomic_expression
-            { '*' operands:atomic_expression }+
+        | expression_type:`mul` operands:atomic_expression { '*' operands:atomic_expression }+
         | atomic_expression
-    ;
+        ;
     atomic_expression =
-        |
-            expression_type:`int_literal`
-            int_literal:INT
-        |
-            expression_type:`reference`
-            variable_name:identifier ~ indices:{ subscript }*
-        |
-            expression_type:`nested`
-            '(' ~ expression:expression ')'
-    ;
-    subscript = '[' @:expression ']' ;
-
-    type_expression = 'int' ~ dimensions:{ '[' ']' }* ;
-
-    identifier = /[a-zA-Z_][0-9a-zA-Z_]*/ ;
-
-    STRING = '"' @:/([^"\n]|\\")*/ '"' ;
-    INT = /0|-?[1-9][0-9]*/ ;
-
+        | expression_type:`int_literal` int_literal:int_literal
+        | expression_type:`reference` variable_name:identifier ~ indices:{ subscript }*
+        | expression_type:`nested` '(' ~ expression:expression ')'
+        ;
+    
+    subscript = '[' @:expression ']';
 """
