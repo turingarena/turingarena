0565dcadd45503f8d5e6792d509c707b6f105a66
Massimo Cairo <cairomassimo@gmail.com>
2018-06-20 16:41:52 +0200

    Prepare CLI project for PyPI publication.

--- cli/setup.py
+++ cli/setup.py
@@ -1,19 +1,8 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 setup(
-    name='turingarena',
-    version='0.0',
-    url='https://github.com/turingarena/turingarena',
-    license='MPL 2.0',
-    author='Massimo Cairo',
-    author_email='cairomassimo@gmail.com',
-    description='',
-    packages=[],
-    py_modules=['turingarena_cli'],
-    entry_points={
-        'console_scripts': [
-            "turingarena=turingarena_cli:turingarena_cli",
-            "turingarenad=turingarena_cli:turingarena_daemon",
-        ],
-    }
+    setup_requires=['pbr'],
+    pbr=True,
 )
