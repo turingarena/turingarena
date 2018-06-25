eeead9fb569d48f0349260b618d058137469f287
Massimo Cairo <cairomassimo@gmail.com>
2018-06-25 16:49:46 +0200

    Improve problem rock_paper_scissor

--- examples/rock_paper_scissor/evaluator.py
+++ examples/rock_paper_scissor/evaluator.py
@@ -1,15 +1,18 @@
-from enum import Enum
-
 from turingarena import *
 
 n_moves = 10
 
+move_names = {
+    0: ("   rock", "\u270a"),
+    1: ("  paper", "\u270b"),
+    2: ("scissor", "\u270c"),
+}
 
-class RPS(Enum):
-    ROCK = 0
-    PAPER = 1
-    SCISSOR = 2
-
+outcome_messages = {
+    0: "draws :|",
+    1: "wins! :)",
+    2: "loses :(",
+}
 
 with run_algorithm(submission.player1) as p1, run_algorithm(submission.player2) as p2:
     players = (p1, p2)
@@ -25,12 +28,7 @@ with run_algorithm(submission.player1) as p1, run_algorithm(submission.player2) 
 
         for j, p in enumerate(players):
             outcome = (moves[j] - moves[1 - j]) % 3
-            message = {
-                0: "draws :|",
-                1: "WINS! :)",
-                2: "loses :(",
-            }[outcome]
-            print(f"Player {j} played {RPS(moves[j]).name:10} and {message}")
+            message = outcome_messages[outcome]
+            move_name, move_symbol = move_names[moves[j]]
+            print(f"    {move_symbol}\t player {j} plays {move_name} and {message}")
             p.procedures.done(i, moves[j], moves[1 - j])
-
-        print()
