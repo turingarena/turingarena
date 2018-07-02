663b30408bab09aed74787e67f33ba550622b16f
alerighi <alessandro.righi@outlook.it>
2018-07-02 15:45:04 +0200

    Cpp evaluator: working for sum of two numbers

--- examples/sum_of_two_numbers/extra_evaluators/evaluator.cpp
+++ examples/sum_of_two_numbers/extra_evaluators/evaluator.cpp
@@ -5,18 +5,27 @@
 #include <unistd.h>
 #include <time.h>
 
+std::string cwd() {
+    char buff[1024];
+    getcwd(buff, sizeof buff);
+    return std::string(buff);
+}
+
 int main() {
     srand(time(nullptr));
+    std::string source{getenv("SUBMISSION_FILE_SOURCE")};
+    std::string interface{cwd() + "/interface.txt"};
 
+    std::cout << "Starting algo with source = " << source << " interface = " << interface << '\n';
     for (int i = 0; i < 10; i++) {
-        turingarena::Algorithm algo{getenv("TURINGARENA_SUBMISSION_SOURCE"), std::string(getcwd(nullptr)) + "/interface.txt"};
+        turingarena::Algorithm algo{source, interface};
 
         int a = rand() % 10;
         int b = rand() % 10;
 
         int c = algo.call_function("sum", a, b);
 
-        std::cout "test case #" << i << ": " << a << " + " << b << " = " << c << "  "
+        std::cout << "test case #" << i << ": " << a << " + " << b << " = " << c << "  ";
         if (c == a + b) {
             std::cout << "CORRECT\n";
         } else {
--- libraries/cpp/turingarena.h
+++ libraries/cpp/turingarena.h
@@ -19,12 +19,12 @@ namespace turingarena {
         // base case
         void put_args() {}
 
-        template <typename First, typename ...Others>
+        template <typename ...Others>
         void put_args(int first, Others ...others)
         {
             driver_downward << "0\n"; // scalar
             driver_downward << first << '\n';
-            put_arg(others...);
+            put_args(others...);
         }
 
         void read_status()
@@ -39,6 +39,7 @@ namespace turingarena {
         {
             driver_downward << "wait\n";
             driver_downward << "0\n";
+            driver_downward.flush();
 
             driver_upward >> time_usage;
             driver_upward >> memory_usage;
@@ -51,12 +52,7 @@ namespace turingarena {
 
             {
                 std::ofstream language_name_pipe{sandbox_dir + "/language_name.pipe"};
-                language_name_pipe << "";
-            }
-
-            {
-                std::ofstream interface_path_pipe{sandbox_dir + "/interface_path.pipe"};
-                interface_path_pipe << interface_path;
+                language_name_pipe << "c++";
             }
 
             {
@@ -64,16 +60,19 @@ namespace turingarena {
                 source_path_pipe << source_path;
             }
 
+
             {
-                std::ifstream sandbox_process_dir_pipe{sandbox_dir + "/sandobox_process_dir.pipe"};
+                std::ofstream interface_path_pipe{sandbox_dir + "/interface_path.pipe"};
+                interface_path_pipe << interface_path;
+            }
+
+            {
+                std::ifstream sandbox_process_dir_pipe{sandbox_dir + "/sandbox_process_dir.pipe"};
                 sandbox_process_dir_pipe >> sandbox_process_dir;
             }
 
-            std::cerr << "Executing algorithm process_dir = " << sandbox_process_dir << std::endl;
-
             driver_downward.open(sandbox_process_dir + "/driver_downward.pipe");
             driver_upward.open(sandbox_process_dir + "/driver_upward.pipe");
-
         }
 
         ~Algorithm()
@@ -81,16 +80,16 @@ namespace turingarena {
             exit();
         }
 
-        template <typename ...Types>
+        template <typename ...Args>
         int call_function(const std::string& function_name, Args ...args)
         {
             int result;
-
+            
             driver_downward << "request\n";
             driver_downward << "call\n";
             driver_downward << function_name << '\n';
             driver_downward << sizeof...(Args) << '\n';
-            put_args(args);
+            put_args(args...);
 
             driver_downward << 1 << '\n'; // 1 = function
             driver_downward << 0 << '\n'; // no callbacks
@@ -105,14 +104,14 @@ namespace turingarena {
             return result;
         }
 
-        template <typename ...Types>
+        template <typename ...Args>
         void call_procedure(const std::string& procedure_name, Args ...args)
         {
             driver_downward << "request\n";
             driver_downward << "call\n";
             driver_downward << procedure_name << '\n';
             driver_downward << sizeof...(Args) << '\n';
-            put_args(args);
+            put_args(args...);
 
             driver_downward << "0\n"; // 0 = procedure
             driver_downward << "0\n"; // no callbacks
