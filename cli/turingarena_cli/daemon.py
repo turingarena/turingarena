23b39850b74c2e9213b499d8ad69282978ce6fab
alerighi <alessandro.righi@outlook.it>
2018-07-26 19:28:57 +0200

    Update turingarena daemon

--- cli/turingarena_cli/daemon.py
+++ cli/turingarena_cli/daemon.py
@@ -5,16 +5,29 @@ import os
 from turingarena_cli.common import *
 
 
-def main():
+def check_root():
+    if os.getuid() != 0:
+        error("You must be root to execute turingarenad!")
+        exit(1)
+
+
+def parse_cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dev-dir", type=str, help="source code directory of TuringArena (for development)")
     parser.add_argument("--daemon", "-d", help="fork turingarena in background", action="store_true")
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+def main():
+    args = parse_cli()
+    check_root()
+
+    ok("Executing Turingarena daemon")
     volumes = []
-    if args.dev_dir is not None:
+    if args.dev_dir:
         dev_dir = os.path.abspath(args.dev_dir)
+        info("Developement mode: mounting {} into the container".format(dev_dir))
         volumes.append("--mount=type=bind,src={},dst=/usr/local/turingarena/,readonly".format(dev_dir))
     cli = [
               "docker",
@@ -31,15 +44,16 @@ def main():
               "TCP-LISTEN:22,fork",
               """EXEC:"/usr/sbin/sshd -q -i -e -o PermitEmptyPasswords=yes -o Protocol=2",nofork""",
           ]
-    ok("Executing Turingarena daemon")
-    info(str(cli))
-
-    if args.daemon:
-        process = subprocess.Popen(cli, cwd="/")
-        ok("Turingarena is running in background with pid {}".format(process.pid))
-    else:
-        ok("Executing turingarena in foreground")
-        os.execvp("docker", cli)
+    info("Running: {}".format(" ".join(cli)))
+    try:
+        if args.daemon:
+            process = subprocess.Popen(cli, cwd="/")
+            ok("Turingarena is running in background with pid {pid}. Use `kill {pid}` to terminate".format(pid=process.pid))
+        else:
+            ok("Executing turingarena in foreground. Use CTRL-C to kill it")
+            os.execvp("docker", cli)
+    except FileNotFoundError:
+        error("docker executable doesn't exist. Is docker installed ?")
 
 
 if __name__ == "__main__":
