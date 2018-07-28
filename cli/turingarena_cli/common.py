76a9f16f60cd9e7531970297d784b51eab487939
alerighi <alessandro.righi@outlook.it>
2018-07-28 21:19:48 +0200

    Update turingarena_cli to support python2.7

--- cli/turingarena_cli/common.py
+++ cli/turingarena_cli/common.py
@@ -2,16 +2,16 @@ from __future__ import print_function
 
 
 def error(string):
-    print(f"\033[1;31m==> ERROR:\033[0m {string}")
+    print("\033[1;31m==> ERROR:\033[0m {}".format(string))
 
 
 def warning(string):
-    print(f"\033[1;33m==> WARNING:\033[0m {string}")
+    print("\033[1;33m==> WARNING:\033[0m {}".format(string))
 
 
 def ok(string):
-    print(f"\033[1;32m==>\033[0m {string}")
+    print("\033[1;32m==>\033[0m {}".format(string))
 
 
 def info(string):
-    print(f"\033[1;34m  ->\033[0m {string}")
+    print("\033[1;34m  ->\033[0m {}".format(string))
