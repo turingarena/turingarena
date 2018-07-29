17ab0c90f8961eec458600a28bab120b2a59cf5e
alerighi <alessandro.righi@outlook.it>
2018-07-29 17:22:43 +0200

    Add random solution to tic_tac_toe

---
+++ examples/tic_tac_toe/players/random.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <ctime>
+
+// Constant declarations
+#define ROWS 3
+#define COLS 3
+#define EMPTY 0
+#define PLAYER 1
+#define OPPONENT 2
+
+// initialize random number generator
+// __attribute__((constructor)) void init() {
+    //srand(time(nullptr));
+// }
+
+// find a random empty cell and place here
+int play_move(int **grid, void place_at(int y, int x)) {
+    int x, y;
+    do {
+        y = rand() % ROWS;
+        x = rand() % COLS;
+    } while (grid[y][x] != EMPTY);
+
+    place_at(y, x);
+    return 42;
+}
+
+void terminate() {}
