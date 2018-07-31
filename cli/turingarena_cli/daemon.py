e05a8dd19648f7499be9c616232ae1f0776228d0
alerighi <alessandro.righi@outlook.it>
2018-07-31 18:09:30 +0200

    Update turingarenad
    Add option --update, -u to update the docker image
    Add option --restart, -r to restart the daemon in already running

--- cli/turingarena_cli/daemon.py
+++ cli/turingarena_cli/daemon.py
@@ -5,16 +5,43 @@ import os
 from turingarena_cli.common import *
 
 
+image = "turingarena/turingarena:latest"
+name = "turingarena"
+
+
+def update_turingarena():
+    ok("Updating turingarena docker image")
+    subprocess.call([
+        "docker",
+        "pull",
+        image,
+    ])
+
+
 def check_root():
     if os.getuid() != 0:
         error("You must be root to execute turingarenad!")
         exit(1)
 
 
+def already_running():
+    cli = ["docker", "ps"]
+    output = subprocess.check_output(cli)
+    return name in str(output)
+
+
+def kill_daemon():
+    ok("Killing turingarena daemon")
+    cli = ["docker", "kill", name]
+    subprocess.call(cli)
+
+
 def parse_cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dev-dir", type=str, help="source code directory of TuringArena (for development)")
     parser.add_argument("--daemon", "-d", help="fork turingarena in background", action="store_true")
+    parser.add_argument("--update", "-u", help="update turingarena docker image", action="store_true")
+    parser.add_argument("--restart", "-r", help="kill daemon if already running", action="store_true")
 
     return parser.parse_args()
 
@@ -22,6 +49,16 @@ def parse_cli():
 def main():
     args = parse_cli()
     check_root()
+
+    if args.update:
+        update_turingarena()
+
+    if already_running():
+        if args.restart:
+            kill_daemon()
+        else:
+            error("Daemon already running, to restart it use --restart option!")
+            exit(1)
 
     ok("Executing Turingarena daemon")
     volumes = []
@@ -32,14 +69,14 @@ def main():
     cli = [
               "docker",
               "run",
-              "--name=turingarena",
+              "--name={}".format(name),
               "--rm",
               "--read-only",
               "--tmpfs=/run/turingarena:exec,mode=1777,uid=1000",
               "--tmpfs=/tmp:exec,mode=1777",
           ] + volumes + [
               "--publish=127.0.0.1:20122:22",
-              "turingarena/turingarena",
+              image,
               "socat",
               "TCP-LISTEN:22,fork",
               """EXEC:"/usr/sbin/sshd -q -i -e -o PermitEmptyPasswords=yes -o Protocol=2",nofork""",
@@ -54,7 +91,3 @@ def main():
             os.execvp("docker", cli)
     except FileNotFoundError:
         error("docker executable doesn't exist. Is docker installed ?")
-
-
-if __name__ == "__main__":
-    main()
