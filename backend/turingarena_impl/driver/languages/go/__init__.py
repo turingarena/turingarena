6f47ceb4d4478e9b06f6dfb9cdf38814bcf5c51b
alerighi <alessandro.righi@outlook.it>
2018-08-01 18:05:37 +0200

    Add go language support

---
+++ backend/turingarena_impl/driver/languages/go/__init__.py
@@ -0,0 +1,13 @@
+from turingarena_impl.driver.language import Language
+
+from .generator import GoSkeletonCodeGen, GoTemplateCodeGen
+from .source import GoAlgorithmSource
+
+language = Language(
+    name="go",
+    extension=".go",
+    source=GoAlgorithmSource,
+    skeleton_generator=GoSkeletonCodeGen,
+    template_generator=GoTemplateCodeGen,
+    supported_for_evaluator=False,
+)
--- cli/turingarena_cli/cli.py
+++ cli/turingarena_cli/cli.py
@@ -229,7 +229,7 @@ def create_make_parser(make_parser):
     make_parser.add_argument("what", help="what to make", default="all",
                              choices=["all", "skeleton", "template", "metadata", "description"])
     make_parser.add_argument("--language", "-l", help="which language to generate", action="append",
-                             choices=["python", "c++", "java"])
+                             choices=["python", "c++", "java", "go"])
     make_parser.add_argument("--print", "-p", help="Print output to stdout instead of writing it to a file", action="store_true")
 
 
