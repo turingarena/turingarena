d029f5248eb4f781b9ced41c4d71c16307298380
Massimo Cairo <cairomassimo@gmail.com>
2018-08-30 16:56:01 +0200

    Implement daemon start/stop as subcommands.

--- cli/turingarena_cli/cli.py
+++ cli/turingarena_cli/cli.py
@@ -4,6 +4,7 @@ import argparse
 import os
 
 from turingarena_cli.common import init_logger
+from turingarena_cli.daemonctl import DaemonControlCommand
 from turingarena_cli.evaluate import EvaluateCommand
 from turingarena_cli.files import FileCommand
 from turingarena_cli.legacy import INFO_PARSER, TEST_PARSER, BASE_MAKE_PARSER, MAKE_PARSER
@@ -22,9 +23,17 @@ except ImportError:
     pass
 
 PARSER = argparse.ArgumentParser(description="Turingarena CLI")
-PARSER.add_argument("--log-level", help="log level", default="WARNING")
+LOG_LEVELS = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", ]
+PARSER.add_argument(
+    "--log-level",
+    help="log level ({})".format(",".join(LOG_LEVELS)),
+    type=str.upper,
+    metavar="LEVEL",
+    choices=LOG_LEVELS,
+    default="WARNING",
+)
 
-subparsers = PARSER.add_subparsers(title="command", dest="command")
+subparsers = PARSER.add_subparsers(dest="command", metavar="COMMAND")
 subparsers.required = True
 
 subparsers.add_parser("evaluate", parents=[EvaluateCommand.PARSER], help=EvaluateCommand.PARSER.description)
@@ -35,6 +44,7 @@ subparsers.add_parser("file", parents=[FileCommand.PARSER], help=FileCommand.PAR
 subparsers.add_parser("make", parents=[MAKE_PARSER], help=MAKE_PARSER.description)
 subparsers.add_parser("skeleton", parents=[BASE_MAKE_PARSER], help="generate skeleton")
 subparsers.add_parser("template", parents=[BASE_MAKE_PARSER], help="generate template")
+subparsers.add_parser("daemon", parents=[DaemonControlCommand.PARSER], help=DaemonControlCommand.PARSER.description)
 
 
 def main():
---
+++ cli/turingarena_cli/daemonctl.py
@@ -0,0 +1,129 @@
+import logging
+import os
+import subprocess
+from argparse import ArgumentParser
+
+from turingarena_cli.command import Command
+
+
+class DaemonControlCommand(Command):
+    PARSER = ArgumentParser(
+        description="Control the execution of the TuringArena daemon",
+        add_help=False,
+    )
+    PARSER.add_argument(
+        "--container-name",
+        help="name of the container to run",
+    )
+    PARSER.add_argument(
+        "--sudo",
+        action="store_true",
+        help="use sudo to run docker",
+    )
+
+    @property
+    def use_sudo(self):
+        return bool(self.args.sudo)
+
+    @property
+    def image(self):
+        return "turingarena/turingarena:latest"
+
+    @property
+    def container_name(self):
+        return self.args.container_name or "turingarena"
+
+
+class DaemonStartCommand(DaemonControlCommand):
+    PARSER = ArgumentParser(
+        description="Start the daemon",
+        add_help=False,
+    )
+    PARSER.add_argument(
+        "--dev-dir",
+        help="source code directory of TuringArena (for development)",
+    )
+    PARSER.add_argument(
+        "--detach",
+        action="store_true",
+        help="Detach from daemon",
+    )
+
+    @property
+    def detach(self):
+        return bool(self.args.detach)
+
+    def run(self):
+        volumes = []
+        if self.args.dev_dir:
+            dev_dir = os.path.abspath(self.args.dev_dir)
+            logging.info("Developement mode: mounting {} into the container".format(dev_dir))
+            volumes.append("--mount=type=bind,src={},dst=/usr/local/turingarena/,readonly".format(dev_dir))
+
+        cli = []
+        if self.use_sudo:
+            cli += ["sudo"]
+        cli += [
+            "docker",
+            "run",
+            "--name={}".format(self.container_name),
+            "--rm",
+        ]
+        if self.detach:
+            cli += ["--detach"]
+        cli += [
+            "--read-only",
+            "--tmpfs=/run/turingarena:exec,mode=1777,uid=1000",
+            "--tmpfs=/tmp:exec,mode=1777",
+        ]
+        cli += volumes
+        cli += [
+            "--publish=127.0.0.1:20122:22",
+            self.image,
+            "socat",
+            "TCP-LISTEN:22,fork",
+            """EXEC:"/usr/sbin/sshd -q -i -e -o PermitEmptyPasswords=yes -o Protocol=2",nofork""",
+        ]
+
+        cli_line = " ".join(cli)
+        logging.info("Running: {}".format(cli_line))
+        print(cli_line)
+        p = subprocess.Popen(cli, cwd="/")
+        p.wait()
+        if self.detach and p.returncode == 0:
+            logging.info("Turingarena is running in background, use `turingarenad --kill` to terminate it")
+
+
+DaemonStartCommand.PARSER.set_defaults(Command=DaemonStartCommand)
+
+
+class DaemonStopCommand(DaemonControlCommand):
+    PARSER = ArgumentParser(
+        description="Stop the daemon",
+        add_help=False,
+    )
+
+    def run(self):
+        logging.info("Killing turingarena daemon")
+        cli = []
+        if self.use_sudo:
+            cli += ["sudo"]
+        cli += ["docker", "kill", self.container_name]
+        p = subprocess.Popen(cli)
+        p.wait()
+
+
+DaemonStopCommand.PARSER.set_defaults(Command=DaemonStopCommand)
+
+subparsers = DaemonControlCommand.PARSER.add_subparsers(metavar="COMMAND")
+subparsers.required = True
+subparsers.add_parser(
+    "start",
+    parents=[DaemonStartCommand.PARSER],
+    help=DaemonStartCommand.PARSER.description,
+)
+subparsers.add_parser(
+    "stop",
+    parents=[DaemonStopCommand.PARSER],
+    help=DaemonStopCommand.PARSER.description,
+)
