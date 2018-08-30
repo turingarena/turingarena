2ad0a8cb422f85e192120c08812ed5050e26b475
Massimo Cairo <cairomassimo@gmail.com>
2018-08-30 18:33:54 +0200

    Do not require 'pbr' when building in Docker.

--- cli/setup.py
+++ cli/setup.py
@@ -3,8 +3,14 @@ import os
 
 from setuptools import setup
 
+use_pbr = 'TURINGARENA_IS_DOCKER' not in os.environ
+if use_pbr:
+    setup_requires = ['pbr']
+else:
+    setup_requires = []
+
 setup(
     name='turingarena-cli',
-    setup_requires=['pbr'],
-    pbr='TURINGARENA_IS_DOCKER' not in os.environ,
+    setup_requires=setup_requires,
+    pbr=use_pbr,
 )
