3d63a77621ebcb2847a31b4d652834e516583cf6
Massimo Cairo <cairomassimo@gmail.com>
2018-09-01 18:25:24 +0200

    Remove pbr from base image.
    [update-base]

--- backend/turingarena_impl/cli_server/evaluate.py
+++ backend/turingarena_impl/cli_server/evaluate.py
@@ -2,7 +2,7 @@ import logging
 import os
 import subprocess
 import sys
-from contextlib import ExitStack, contextmanager
+from contextlib import ExitStack
 from tempfile import TemporaryDirectory
 
 from turingarena_common.commands import LocalExecutionParameters, EvaluateCommandParameters
@@ -27,36 +27,18 @@ def evaluate_cmd(parameters: EvaluateCommandParameters, local_execution: LocalEx
             ))
             output = jq.stdin
 
-        files = stack.enter_context(parse_files(parameters.file, ["source"]))
-        logging.info("Submitted files")
-        for name, path in files.items():
-            logging.info(f"{name}: {path}")
+        files = {}
+        with TemporaryDirectory() as temp_dir:
+            for name, submission_file in parameters.submission.items():
+                dirpath = os.path.join(temp_dir, name)
+                os.mkdir(dirpath)
+                path = os.path.join(dirpath, submission_file.filename)
+                with open(path, "xb") as f:
+                    f.write(submission_file.content)
+                files[name] = path
+            logging.info(f"Submission fields: {list(parameters.submission.keys())}")
 
-        evaluator = Evaluator.get_evaluator(parameters.evaluator)
-        logging.info(f"Running evaluator: {evaluator}")
-        for event in evaluator.evaluate(files=files):
-            print(event, file=output, flush=True)
-
-
-@contextmanager
-def parse_files(files, default_fields):
-    default_fields = iter(default_fields)
-    with TemporaryDirectory() as temp_dir:
-        yield dict(
-            parse_file(arg, temp_dir, default_fields)
-            for arg in files
-        )
-
-
-def parse_file(file, temp_dir, default_fields):
-    if ":" in file:
-        name, path = file.split(":", 1)
-    elif "=" in file:
-        name, value = file.split("=", 1)
-        path = os.path.join(temp_dir, name + ".txt")
-        with open(path, "x") as f:
-            f.write(value)
-    else:
-        name = next(default_fields)
-        path = file
-    return name, path
+            evaluator = Evaluator.get_evaluator(parameters.evaluator)
+            logging.info(f"Running evaluator: {evaluator}")
+            for event in evaluator.evaluate(files=files):
+                print(event, file=output, flush=True)
