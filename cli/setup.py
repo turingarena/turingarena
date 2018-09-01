4a009d635f8672fc04cffab572893e80d1c4fa3e
Massimo Cairo <cairomassimo@gmail.com>
2018-09-01 15:56:25 +0200

    Add PBR to base image for CLI installation.
    [update-base]

--- cli/setup.py
+++ cli/setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
-import os
 
 from setuptools import setup
 
-use_pbr = 'TURINGARENA_IS_DOCKER' not in os.environ
-if use_pbr:
-    setup_requires = ['pbr']
-else:
-    setup_requires = []
-
 setup(
     name='turingarena-cli',
-    setup_requires=setup_requires,
-    pbr=use_pbr,
+    setup_requires=['pbr'],
+    pbr=True,
 )
