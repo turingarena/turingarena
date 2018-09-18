3afa089ac86a8ba9975e72ff90a87c20220c95c9
Massimo Cairo <cairomassimo@gmail.com>
2018-09-18 14:36:59 +0200

    Travis: move hyper deployment to Docker

---
+++ deploy/hypersh-deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+true \
+    && hyper \
+        config \
+        --default-region=us-west-1 \
+        --accesskey=$HYPERSH_ACCESS_KEY \
+        --secretkey=$HYPERSH_SECRET_KEY \
+    && hyper pull $DOCKER_IMAGE \
+    && HYPERSH_DANGLING_IMAGES=$(hyper images -q --filter "dangling=true") \
+    && echo HYPERSH_DANGLING_IMAGES=$HYPERSH_DANGLING_IMAGES >&2 \
+    && hyper rmi $HYPERSH_DANGLING_IMAGES || true \
+    && hyper func rm $HYPERSH_FUNC_NAME || true \
+    && hyper func create \
+        --name $HYPERSH_FUNC_NAME \
+        --env AWS_DEFAULT_REGION=us-east-1 \
+        --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        $DOCKER_IMAGE \
+        python -m turingarena_impl.api.hypersh_evaluate \
+    && export HYPERSH_FUNC_ID=$(hyper func inspect $HYPERSH_FUNC_NAME | jq -r .[0].UUID) \
+    && echo $HYPERSH_FUNC_ID \
+    && true
