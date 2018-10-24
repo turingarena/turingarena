43d02c34b7254ec5b920f95e8ee8a296b4d59735
Massimo Cairo <cairomassimo@gmail.com>
2018-10-24 11:26:51 +0200

    Drop SubmissionsTable.

--- backend/serverless-test-locally.sh
+++ backend/serverless-test-locally.sh
@@ -2,5 +2,4 @@
 
 HYPERSH_REGION=us-west-1 \
 DYNAMODB_EVALUATION_EVENTS_TABLE=turingarena-branch-develop-EvaluationEventsTable \
-DYNAMODB_SUBMISSIONS_TABLE=turingarena-branch-develop-SubmissionsTable \
 python -m turingarena_impl.api.serve
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -22,7 +22,6 @@ true \
         --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
         --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
         --env DYNAMODB_EVALUATION_EVENTS_TABLE=turingarena-$SERVERLESS_STAGE-EvaluationEventsTable \
-        --env DYNAMODB_SUBMISSIONS_TABLE=turingarena-$SERVERLESS_STAGE-SubmissionsTable \
         --env S3_FILES_BUCKET=turingarena-$SERVERLESS_STAGE-files \
         $DOCKER_IMAGE \
         python -m turingarena_impl.api.hypersh_evaluate \
