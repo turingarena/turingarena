c4b4480840ed66f1e2c28d0b687960bc6c9e1310
Massimo Cairo <cairomassimo@gmail.com>
2018-11-04 00:55:36 +0100

    Fix 'turingarena file cat' command

--- backend/turingarena_impl/cli_server/files.py
+++ backend/turingarena_impl/cli_server/files.py
@@ -1,31 +1,24 @@
 import os
-from contextlib import ExitStack
-
 import sys
 
 from turingarena_common.commands import FileCommandParameters, LocalExecutionParameters, FileCatCommandParameters
+
 from turingarena_impl.cli_server.pack import create_working_directory
 from turingarena_impl.file.generated import PackGeneratedDirectory
 
 
 def file_cmd(parameters: FileCommandParameters, local_execution: LocalExecutionParameters):
-    with ExitStack() as stack:
-        work_dir = stack.enter_context(create_working_directory(
-            parameters.working_directory,
-            local_execution=local_execution,
-        ))
-
-        command = parameters.command
-        for t, runner in COMMANDS_MAP.items():
-            if isinstance(command, t):
-                return runner(command, work_dir, parameters.working_directory.current_directory)
-        raise TypeError(type(command))
+    command = parameters.command
+    for t, runner in COMMANDS_MAP.items():
+        if isinstance(command, t):
+            return runner(command, parameters.working_directory, local_execution=local_execution)
+    raise TypeError(type(command))
 
 
-def file_cat_cmd(parameters: FileCatCommandParameters, working_directory, current_directory):
-    with create_working_directory(working_directory) as work_dir:
+def file_cat_cmd(parameters: FileCatCommandParameters, working_directory, local_execution):
+    with create_working_directory(working_directory, local_execution=local_execution) as work_dir:
         generated = PackGeneratedDirectory(work_dir)
-        path = os.path.join(current_directory, parameters.path)
+        path = os.path.join(working_directory.current_directory, parameters.path)
         generated.cat_file(path, file=sys.stdout)
 
 
