a13d86308b6eb8ff8620c1ef732f995d21e7d5ba
alerighi <alessandro.righi@outlook.it>
2018-11-05 10:46:45 +0100

    Fix text FileNotFoundError

--- src/turingarena_impl/file/generated.py
+++ src/turingarena_impl/file/generated.py
@@ -7,6 +7,7 @@ from turingarena_impl.driver.language import Language
 from turingarena_impl.text.parser import TextParser
 
 INTERFACE_TXT = "interface.txt"
+TEXT_FILENAMES = ["statement.md", "README.md"]
 
 
 class PackGeneratedDirectory(namedtuple("PackGeneratedDirectory", ["work_dir"])):
@@ -25,32 +26,29 @@ class PackGeneratedDirectory(namedtuple("PackGeneratedDirectory", ["work_dir"]))
     def _generate_targets(self):
         for dirpath, dirnames, filenames in os.walk(self.work_dir):
             relpath = os.path.relpath(dirpath, self.work_dir)
+            descriptions = {}
+            for file in TEXT_FILENAMES:
+                if file in filenames:
+                    text_path = os.path.join(dirpath, file)
+                    descriptions = TextParser(text_path).descriptions
+                    yield (os.path.join(relpath, TEXT_FILENAMES[0]), self._create_text_generator(text_path))
             if INTERFACE_TXT in filenames:
-                yield from self._generate_interface_targets(dirpath, relpath)
-            if "statement.md" in filenames:
-                path = os.path.join(dirpath, "statement.md")
-                yield (os.path.join(relpath, "statement.md"), self._create_text_generator(path))
-            elif "README.md" in filenames:
-                path = os.path.join(dirpath, "README.md")
-                yield (os.path.join(relpath, "statement.md"), self._create_text_generator(path))
+                yield from self._generate_interface_targets(dirpath, relpath, descriptions)
 
-    def _generate_interface_targets(self, abspath, relpath):
+    def _generate_interface_targets(self, abspath, relpath, descriptions):
         for lang in Language.languages():
             interface_path = os.path.join(abspath, INTERFACE_TXT)
-            statement_path = os.path.join(abspath, "README.md")
             yield (
                 os.path.join(relpath, f"skeleton{lang.extension}"),
-                self._create_interface_code_generator(interface_path, None, lang.skeleton_generator()),
+                self._create_interface_code_generator(interface_path, descriptions, lang.skeleton_generator()),
             )
             yield (
                 os.path.join(relpath, f"template{lang.extension}"),
-                self._create_interface_code_generator(interface_path, statement_path, lang.template_generator()),
+                self._create_interface_code_generator(interface_path, descriptions, lang.template_generator()),
             )
 
-    def _create_interface_code_generator(self, interface_path, statement_path, code_generator):
+    def _create_interface_code_generator(self, interface_path, descriptions, code_generator):
         def generator(outfile):
-            descriptions = TextParser(statement_path).descriptions if statement_path is not None else {}
-
             with open(interface_path) as f:
                 interface = InterfaceDefinition.compile(f.read(), descriptions=descriptions)
 
