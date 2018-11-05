8cc9ad2003aa327aeb2a4d072cf12bdd71bab1ca
Massimo Cairo <cairomassimo@gmail.com>
2018-11-05 22:12:37 +0100

    Drop daemon support for 'turingarena file cat'.

--- src/turingarena_impl/cli_server/pack.py
+++ src/turingarena_impl/cli_server/pack.py
@@ -1,23 +1,9 @@
 import logging
-import os
 from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 
 from turingarena_common.commands import WorkingDirectory, LocalExecutionParameters
 from turingarena_impl.cli_server.git_manager import GitManager
-
-
-@contextmanager
-def enter_working_directory(working_directory: WorkingDirectory, *, local_execution):
-    with create_working_directory(working_directory, local_execution=local_execution) as work_dir:
-        old_cwd = os.curdir
-
-        os.chdir(work_dir)
-        os.chdir(working_directory.current_directory)
-
-        yield
-
-        os.chdir(old_cwd)
 
 
 @contextmanager
