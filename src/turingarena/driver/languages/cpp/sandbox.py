a2d755eb199a62bd8367d4f19042924bc70fb8e0
Massimo Cairo <cairomassimo@gmail.com>
2018-11-06 05:57:35 +0100

    Drop testing code from sandbox.

--- src/turingarena/driver/languages/cpp/sandbox.py
+++ src/turingarena/driver/languages/cpp/sandbox.py
@@ -26,27 +26,6 @@ def init_sandbox():
 
 
 def main():
-    a = [None] * 5000000
-    del a
-
-    rusage = resource.getrusage(resource.RUSAGE_SELF)
-    print(f"FROM SANDBOX BEFORE: {rusage.ru_maxrss}", file=sys.stderr)
-
-    pid = os.getpid()
-
-    cmd = f"echo 5 > /proc/{pid}/clear_refs"
-    print(f"FROM SANDBOX: {cmd}", file=sys.stderr)
-    os.system(cmd)
-
-    rusage = resource.getrusage(resource.RUSAGE_SELF)
-    print(f"FROM SANDBOX AFTER:  {rusage.ru_maxrss}", file=sys.stderr)
-
-    a = [None] * 10000000
-    del a
-
-    rusage = resource.getrusage(resource.RUSAGE_SELF)
-    print(f"FROM SANDBOX NOW:  {rusage.ru_maxrss}", file=sys.stderr)
-
     init_sandbox()
 
     # From here every system call that is not allowed will result in an Exception
