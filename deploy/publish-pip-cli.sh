37725edbedbe1e7e3c2292d0ae4c847579c8b652
Massimo Cairo <cairomassimo@gmail.com>
2018-11-06 12:19:27 +0100

    Maybe fix deploy via Pip.

--- deploy/publish-pip-cli.sh
+++ deploy/publish-pip-cli.sh
@@ -1,7 +1,9 @@
 set -xe
 
-echo "VERSION='$CLI_VERSION'" > /src/cli/turingarena_cli/build_version.py
-
 cd /src/cli
+
+echo "$CLI_VERSION" > version.txt
+echo "VERSION='$CLI_VERSION'" > turingarena_cli/build_version.py
+
 python3 setup.py egg_info bdist_wheel
 twine upload dist/*
--- deploy/publish-pip-dev.sh
+++ deploy/publish-pip-dev.sh
@@ -1,7 +1,9 @@
 set -xe
 
-echo "VERSION='$CLI_VERSION'" > /src/src/turingarena/build_version.py
-
 cd /src/src
+
+echo "$CLI_VERSION" > version.txt
+echo "VERSION='$CLI_VERSION'" > turingarena/build_version.py
+
 python3 setup.py egg_info bdist_wheel
 twine upload dist/*
