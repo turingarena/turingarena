33255e63ae704b1eccca16b0e4d893930f779ea6
alerighi <alessandro.righi@outlook.it>
2018-11-10 13:44:52 +0100

    Evaluation: add a warning if a goal is used but not declared

--- src/turingarena/goals.py
+++ src/turingarena/goals.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from typing import MutableMapping
 
@@ -5,9 +6,10 @@ from typing import MutableMapping
 class Goals(MutableMapping):
     PATTERN = re.compile("[a-z][a-z0-9_]*")
 
-    def __init__(self, on_assign_goal):
+    def __init__(self, on_assign_goal, declared_goals):
         self._assigned_goals = {}
         self._on_assign_goal = on_assign_goal
+        self._declared_goals = declared_goals
 
     def __getitem__(self, item):
         return self._assigned_goals[item]
@@ -16,6 +18,9 @@ class Goals(MutableMapping):
         assert isinstance(key, str)
         assert self.PATTERN.match(key)
         assert value in (True, False)
+        if key not in self._declared_goals:
+            logging.warning(f"goal '{key}' is not declared in turingarena.toml!\n" 
+                            "This goal will not be registered by the web interface!")
         try:
             assert self._assigned_goals[key] == value, (
                 f"goal {key} was already assigned to {self._assigned_goals[key]}"
