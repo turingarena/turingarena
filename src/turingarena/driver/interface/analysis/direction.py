2446a6a18d4fa135a507a9639c273828b5ef01c4
Massimo Cairo <cairomassimo@gmail.com>
2018-11-18 16:57:05 +0100

    Driver: move analysis classes to specific package.

---
+++ src/turingarena/driver/interface/analysis/direction.py
@@ -0,0 +1,56 @@
+from turingarena.driver.interface.statements.call import MethodReturnNode
+from turingarena.driver.interface.statements.callback import CallbackCallNode
+from turingarena.driver.interface.statements.io import Read, Checkpoint
+from turingarena.driver.interface.variables import ReferenceDirection
+from turingarena.util.visitor import visitormethod
+
+
+class DeclarationDirectionAnalyzer:
+    def declaration_directions(self, n):
+        return frozenset(self._get_directions(n))
+
+    DIRECTION_MAP = {
+        ReferenceDirection.DOWNWARD: [
+            Read,
+        ],
+        ReferenceDirection.UPWARD: [
+            Checkpoint,
+            CallbackCallNode,
+            MethodReturnNode,
+        ],
+    }
+
+    @visitormethod
+    def _get_directions(self, n):
+        pass
+
+    def _get_directions_SequenceNode(self, n):
+        for child in n.children:
+            yield from self.declaration_directions(child)
+
+    def _get_directions_For(self, n):
+        return self.declaration_directions(n.body)
+
+    def _get_directions_Loop(self, n):
+        return self.declaration_directions(n.body)
+
+    def _get_directions_If(self, n):
+        for b in n.branches:
+            yield from self.declaration_directions(b)
+
+    def _get_directions_Switch(self, n):
+        for c in n.cases:
+            yield from self.declaration_directions(c.body)
+
+    def _get_directions_MethodCallbacksNode(self, n):
+        for callback in n.callbacks:
+            yield from self.declaration_directions(callback.body)
+
+    def _get_directions_CallbackImplementation(self, n):
+        return self.declaration_directions(n.body)
+
+    def _get_directions_IntermediateNode(self, n):
+        for d, ts in self.DIRECTION_MAP.items():
+            for t in ts:
+                if isinstance(n, t):
+                    yield d
