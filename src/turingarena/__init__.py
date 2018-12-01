3dc9fa56b188de88a738e7bafdb6f68739206719
Massimo Cairo <cairomassimo@gmail.com>
2018-12-01 17:13:54 +0100

    Revert "Remove some useless files from previous merge."
    This reverts commit 239ec42c7ae0007de33ba4e7edbd7753d35cd6df.

---
+++ src/turingarena/__init__.py
@@ -0,0 +1,10 @@
+from turingarena.evallib.algorithm import run_algorithm
+import turingarena.evallib.evaluation
+import turingarena.evallib.goals
+import turingarena.evallib.metadata
+import turingarena.evallib.parameters
+from turingarena.driver.client.exceptions import *
+
+submission = turingarena.evallib.evaluation.Submission()
+parameters = turingarena.evallib.parameters.Parameters()
+goals = turingarena.evallib.goals.Goals()
