dfdc2113e611427719485efc9beeb74f1cb67333
Massimo Cairo <cairomassimo@gmail.com>
2018-12-02 16:40:16 +0100

    Fix issue when processing comments in HTML.

--- src/turingarena/text/visitor.py
+++ src/turingarena/text/visitor.py
@@ -1,10 +1,10 @@
 class TextVisitor:
     def visit(self, el):
-        method_name = "visit_" + el.tag
-        if hasattr(self, method_name):
-            return getattr(self, method_name)(el)
-        else:
-            return self.generic_visit(el)
+        if isinstance(el.tag, str):
+            method_name = "visit_" + el.tag
+            if hasattr(self, method_name):
+                return getattr(self, method_name)(el)
+        return self.generic_visit(el)
 
     def generic_visit(self, el):
         raise NotImplementedError
