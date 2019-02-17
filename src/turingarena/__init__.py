2d943c2242b369ba64fe024086e638a45c41f9dc
Massimo Cairo <cairomassimo@gmail.com>
2019-02-17 10:54:15 +0100

    Rename methods: ta.evaluation.{send_data,send_file}.

--- src/turingarena/__init__.py
+++ src/turingarena/__init__.py
@@ -1,9 +1,8 @@
 from turingarena.evallib.algorithm import run_algorithm
-import turingarena.evallib.evaluation
+import turingarena.evallib.evaluation as evaluation
 import turingarena.evallib.goals
 import turingarena.evallib.metadata
 import turingarena.evallib.parameters
-from turingarena.evallib.evaluation import send_file_as_path
 from turingarena.evallib.tempdir import get_temp_dir
 from turingarena.driver.client.exceptions import *
 
