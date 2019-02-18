c5e9190bd47a71a17dc281146b519426b1d8815b
alerighi <alessandro.righi@outlook.it>
2019-02-18 15:57:42 +0100

    [taserver] Changes in setup.cfg

--- taserver/setup.py
+++ taserver/setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup
 
-setup(
-    install_requires=['flask', 'psycopg2-binary', 'bcrypt', 'pytest', 'turingarena-dev', 'commonmark', 'tabulate',
-                      'toml', 'coloredlogs'],
-    tests_require=['pytest', 'pytest-flask'],
-    setup_requires=['pytest_runner']
-)
+setup()
