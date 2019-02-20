6de41c07a40b0b3a1467ed3a5326c993631aca58
alerighi <alessandro.righi@outlook.it>
2019-02-20 21:15:56 +0100

    [C language] Build proper skeleton in pure C

--- src/turingarena/driver/languages/c/generator.py
+++ src/turingarena/driver/languages/c/generator.py
@@ -3,21 +3,23 @@ from turingarena.driver.languages.cpp import CppCodeGen
 
 class CCodeGen(CppCodeGen):
     def visit_Interface(self, n):
-        self.line("#include <cstdio>")
-        self.line("#include <cstdlib>")
-        self.line("#include <cassert>")
+        self.line("#include <stdio.h>")
+        self.line("#include <stdlib.h>")
         self.line()
         for c in n.constants:
             self.visit(c)
             self.line()
-        self.line('extern "C" {')
         self.line()
         for m in n.methods:
             self.line(f"{self.visit(m)};")
             self.line()
-        self.line('} // extern "C"')
         self.line()
         self.line("int main() {")
         with self.indent():
             self.visit(n.main)
         self.line("}")
+
+    def visit_Alloc(self, n):
+        reference = self.visit(n.reference)
+        size = self.visit(n.size)
+        self.line(f"{reference} = malloc({size} * sizeof(*{reference}));")
--- src/turingarena/driver/languages/c/runner.py
+++ src/turingarena/driver/languages/c/runner.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import subprocess
 
@@ -9,10 +10,35 @@ logger = logging.getLogger(__name__)
 class CProgramRunner(CppProgramRunner):
     def _compile_source(self):
         cli = [
-            "gcc", "-c", "-O2", "-std=c11", "-Wall",
+            *self._ccache(), "gcc", "-c", "-O2", "-std=c11", "-Wall",
             "-o", self._source_object_path,
-            self.program.source_path
+            self.program.source_path,
         ]
 
         logger.debug("Compiling source: " + " ".join(cli))
         subprocess.run(cli, universal_newlines=True, check=True)
+
+    def _compile_skeleton(self):
+        cli = [
+            *self._ccache(), "gcc", "-c", "-O2", "-std=c++17", "-Wno-unused-result",
+            "-o", self._skeleton_object_path,
+            self._skeleton_path,
+        ]
+
+        logging.debug("Compiling skeleton: " + " ".join(cli))
+        subprocess.run(cli, universal_newlines=True, check=True)
+
+    def _link_executable(self):
+        cli = [
+            *self._ccache(), "gcc", "-static",
+            "-o", self.executable_path,
+            self._skeleton_object_path,
+            self._source_object_path,
+        ]
+
+        logging.debug("Linking executable: " + " ".join(cli))
+        subprocess.run(cli, universal_newlines=True, check=True)
+
+    @property
+    def _skeleton_path(self):
+        return os.path.join(self.temp_dir, "skeleton.c")
