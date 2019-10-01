ceaf1b315a7517fe4b43d736a2d81d2c47044d60
Massimo Cairo <cairomassimo@gmail.com>
2019-10-02 00:35:15 +0200

    contrib-task-maker: return UI messages as iterator

--- rust/turingarena-contrib-task-maker/examples/runner.rs
+++ rust/turingarena-contrib-task-maker/examples/runner.rs
@@ -2,6 +2,9 @@ use turingarena_contrib_task_maker::*;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
-    let p: std::path::PathBuf = args[1].clone().into();
-    run_task(&p);
+    let (server, messages) = run_task(args[1].clone());
+    for m in messages {
+        println!("{:?}", m);
+    }
+    server.join().unwrap();
 }
