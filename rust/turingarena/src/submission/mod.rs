8c2a1e8d6557f872e977394bee63feddba3ca4a0
Massimo Cairo <cairomassimo@gmail.com>
2019-10-04 17:00:18 +0200

    submission: create metadata types

--- rust/turingarena/src/submission/mod.rs
+++ rust/turingarena/src/submission/mod.rs
@@ -1,3 +1,36 @@
+extern crate serde;
+
+use super::content::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct FieldId(pub String);
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Field {
+    pub id: FieldId,
+    pub title: Text,
+    pub types: Vec<FileType>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct FileTypeId(pub String);
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct FileTypeExtension(pub String);
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct FileType {
+    pub id: FileTypeId,
+    pub title: Text,
+    pub extensions: Vec<FileTypeExtension>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Form {
+    pub fields: Vec<Field>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
