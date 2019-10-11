d0b573c118bb6d5588591057df8e05b129736aae
Massimo Cairo <cairomassimo@gmail.com>
2019-10-11 19:25:20 +0200

    contest-server: add user management

--- rust/turingarena-contest-server/src/main.rs
+++ rust/turingarena-contest-server/src/main.rs
@@ -4,10 +4,23 @@
 extern crate diesel;
 extern crate juniper;
 extern crate juniper_rocket;
+extern crate rand;
 extern crate rocket;
 
 use diesel::prelude::*;
-use juniper::FieldResult;
+use juniper::{FieldError, FieldResult};
+
+const DDL: &str = "
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+CREATE TABLE tasks (
+    name TEXT PRIMARY KEY
+);
+CREATE TABLE users (
+    id TEXT PRIMARY KEY,
+    display_name TEXT
+);
+";
 
 struct Context {}
 impl juniper::Context for Context {}
@@ -44,29 +57,90 @@ impl Task {
     }
 }
 
+table! {
+    users (id) {
+        id -> Text,
+        display_name -> Text,
+    }
+}
+
+#[derive(Insertable, juniper::GraphQLInputObject)]
+#[table_name = "users"]
+struct UserInput {
+    pub id: Option<String>,
+    pub display_name: Option<String>,
+}
+
+#[derive(Queryable)]
+struct User {
+    pub id: String,
+    pub display_name: String,
+}
+
+/// A task
+#[juniper::object(Context = Context)]
+impl User {
+    fn id(&self) -> String {
+        return self.id.clone();
+    }
+
+    /// Name of this task. Unique in the current contest.
+    fn display_name(&self) -> String {
+        return self.display_name.clone();
+    }
+}
+
+struct MutationOk;
+
+#[juniper::object]
+impl MutationOk {
+    fn ok() -> bool {
+        true
+    }
+}
+
 struct Contest;
 
 #[juniper::object(Context = Context)]
 impl Contest {
+    fn users(context: &Context) -> FieldResult<Vec<User>> {
+        // TODO: check admin credentials
+        let connection = context.connect_db()?;
+        return Ok(users::table.load::<User>(&connection)?);
+    }
+
     fn tasks(context: &Context) -> FieldResult<Vec<Task>> {
         let connection = context.connect_db()?;
         return Ok(tasks::table.load::<Task>(&connection)?);
     }
 
-    fn set_tasks(context: &Context, tasks: Vec<TaskInput>) -> FieldResult<Vec<Task>> {
+    fn configure(
+        context: &Context,
+        tasks: Vec<TaskInput>,
+        users: Vec<UserInput>,
+    ) -> FieldResult<MutationOk> {
+        // TODO: check admin credentials
         let connection = context.connect_db()?;
-        connection.execute(
-            "
-            DROP TABLE IF EXISTS tasks;
-            CREATE TABLE tasks (
-                name TEXT PRIMARY KEY
-            );
-            ",
-        )?;
-        diesel::insert_into(tasks::table)
-            .values(tasks)
-            .execute(&connection)?;
-        return Ok(tasks::table.load::<Task>(&connection)?);
+        connection
+            .transaction::<_, diesel::result::Error, _>(|| {
+                connection.execute(DDL)?;
+                diesel::insert_into(users::table)
+                    .values(
+                        users
+                            .into_iter()
+                            .map(|u| UserInput {
+                                id: u.id.or(Some(format!("user-{}", rand::random::<u64>()))),
+                                ..u
+                            })
+                            .collect::<Vec<_>>(),
+                    )
+                    .execute(&connection)?;
+                diesel::insert_into(tasks::table)
+                    .values(tasks)
+                    .execute(&connection)?;
+                Ok(MutationOk)
+            })
+            .map_err(|e| FieldError::from(e))
     }
 }
 
