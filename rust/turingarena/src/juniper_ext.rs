9d5d17b0b163ac7daf862bc8bcbc8807bb77d95d
Massimo Cairo <cairomassimo@gmail.com>
2019-10-19 12:03:57 +0200

    juniper_ext: add `graphql_derive_union_from_enum!`

---
+++ rust/turingarena/src/juniper_ext.rs
@@ -0,0 +1,31 @@
+macro_rules! graphql_derive_union_from_enum {
+    (
+        $( $enum:tt )*
+    ) => {
+        $( $enum )*
+        graphql_union_from_enum! { $( $enum )* }
+    };
+}
+
+macro_rules! graphql_union_from_enum {
+    (
+        $( #[ $enum_attr:meta ] )* $vis:vis enum $enum_ident:ident {
+            $(
+                $( #[ $variant_attr:meta ] )*
+                $variant_ident:ident ( $delegate:ident )
+            )*
+
+            $( , )?
+        }
+    ) => {
+        $crate::juniper::graphql_union!{
+            $enum_ident: () as (stringify!($enum_ident)) where Scalar = <S> |&self| {
+                instance_resolvers: |_| {
+                    $(
+                        &$delegate => match self { $enum_ident::$variant_ident(delegate) => Some(delegate) },
+                    ),*
+                }
+            }
+        }
+    };
+}
