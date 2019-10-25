8567dd80d681c80677caf78adeba99b8a4ee23f6
Massimo Cairo <cairomassimo@gmail.com>
2019-10-26 00:27:49 +0200

    web: refresh contest query on submit

--- web/projects/turingarena-contest/src/app/relative-time.pipe.ts
+++ web/projects/turingarena-contest/src/app/relative-time.pipe.ts
@@ -8,6 +8,7 @@ export class RelativeTimePipe implements PipeTransform {
   constructor(@Inject(LOCALE_ID) private locale: string) { }
 
   transform(value: string): string {
-    return DateTime.fromISO(value).setLocale(this.locale).toRelative() || '';
+    const time = DateTime.fromISO(value).setLocale(this.locale);
+    return time.diffNow().as('seconds') > -10 ? 'now' : (time.toRelative() || '');
   }
 }
