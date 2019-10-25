b970ef236ae33663f77852ddbfd6f64f8caaf68c
Massimo Cairo <cairomassimo@gmail.com>
2019-10-26 01:17:31 +0200

    web: open submission detail from list

--- web/projects/turingarena-contest/src/app/submission-list-dialog/submission-list-dialog.component.ts
+++ web/projects/turingarena-contest/src/app/submission-list-dialog/submission-list-dialog.component.ts
@@ -5,6 +5,8 @@ import { SubmissionListQueryService } from '../submission-list-query.service';
 import { QueryRef } from 'apollo-angular';
 import { SubmissionListQuery, SubmissionListQueryVariables, SubmissionListQuery_problems, SubmissionListQuery_problems_submissions } from '../__generated__/SubmissionListQuery';
 import { ContestQuery_problems_material, ContestQuery_problems_material_scorables } from '../__generated__/ContestQuery';
+import { SubmissionDialogComponent } from '../submission-dialog/submission-dialog.component';
+import { SubmissionQueryService } from '../submission-query.service';
 
 @Component({
   selector: 'app-submission-list-dialog',
@@ -16,6 +18,7 @@ export class SubmissionListDialogComponent implements OnInit {
   constructor(
     private modal: NgbModal,
     private submissionListQueryService: SubmissionListQueryService,
+    private submissionQueryService: SubmissionQueryService,
   ) { }
 
   @Input()
@@ -34,6 +37,23 @@ export class SubmissionListDialogComponent implements OnInit {
 
   getState(scorable: ContestQuery_problems_material_scorables, submission: SubmissionListQuery_problems_submissions) {
     return submission.scorables.find((s) => s.scorableId === scorable.name) || { score: 0 };
+  }
+
+  async openDetail(submission: SubmissionListQuery_problems_submissions) {
+    const modalRef = this.modal.open(SubmissionDialogComponent);
+    const modal = modalRef.componentInstance;
+
+    modal.appComponent = this.appComponent;
+    modal.problemName = this.problemName;
+    modal.setSubmissionQueryRef(this.submissionQueryService.watch({
+      submissionId: submission.id,
+    }));
+
+    try {
+      await modalRef.result;
+    } catch (e) {
+      // No-op
+    }
   }
 
 }
