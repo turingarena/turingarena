caa206c5d7d80cfb3fdc9d1588fbf7b1f4f143b2
alerighi <alerighi4@gmail.com>
2019-10-26 14:46:47 +0200

    Update start script

--- rust/turingarena-contest/examples/example-contest/start.sh
+++ rust/turingarena-contest/examples/example-contest/start.sh
@@ -1,8 +1,12 @@
+#!/bin/bash 
+
 set -ex
-( cd easy1/ && task-maker )
 ( cd easy1/testo/ && rm -f testo.pdf && latexmk -pdf testo.tex )
 rm -f database.sqlite3
 cargo run -- init-db "Test contest"
+for i in {1..10}; do 
+    cargo run -- add-user "utente$i" "Utente $i" "utente$i"
+done
 cargo run -- add-user test Test test
 cargo run -- add-problem easy1 $PWD/easy1
 cargo run -- serve --skip-auth 1
