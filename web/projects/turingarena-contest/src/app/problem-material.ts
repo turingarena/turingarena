ba579c46ee23203fc9dd40b5e5325c1ad9b8194d
Massimo Cairo <cairomassimo@gmail.com>
2019-11-01 11:11:14 +0100

    web: use GraphQL fragments for problem material

--- web/projects/turingarena-contest/src/app/problem-material.ts
+++ web/projects/turingarena-contest/src/app/problem-material.ts
@@ -2,130 +2,136 @@ import gql from 'graphql-tag';
 
 import { ProblemMaterialFragment } from './__generated__/ProblemMaterialFragment';
 import { textFragment } from './text';
-import { scoreRangeFragment } from './score';
 
 export const problemMaterialFragment = gql`
-  fragment ProblemMaterialFragment on Problem {
-    material {
-      title {
-        ...TextFragment
-      }
-      statement {
-        name
-        type
-        content {
-          base64
-        }
-      }
-      attachments {
-        title {
-          ...TextFragment
-        }
-        file {
-          name
-          type
-          content {
-            base64
-          }
-        }
-      }
-      submissionForm {
-        fields {
-          id
-          title {
-            ...TextFragment
-          }
-          types {
-            id
-            title {
-              ...TextFragment
-            }
-            extensions
-          }
-        }
-      }
-      awards {
-        name
-        title {
-          ...TextFragment
-        }
-        content {
-          __typename
-          ... on ScoreAwardContent {
-            range {
-              ...ScoreRangeFragment
-            }
-          }
-        }
-      }
-      feedback {
-        __typename
-        ... on TableSection {
-          caption {
-            ...TextFragment
-          }
-          cols {
-            title {
-              ...TextFragment
-            }
-            content {
-              __typename
-              ... on ScoreColContent {
-                range {
-                  ...ScoreRangeFragment
-                }
-              }
-            }
-          }
-          rowGroups {
-            title {
-              ...TextFragment
-            }
-            rows {
-              content
-              cells {
-                content {
-                  __typename
-                  ... on RowNumberCellContent {
-                    number
-                  }
-                  ... on RowTitleCellContent {
-                    title {
-                      ...TextFragment
-                    }
-                  }
-                  ... on ScoreCellContent {
-                    key
-                    range {
-                      ...ScoreRangeFragment
-                    }
-                  }
-                  ... on MessageCellContent {
-                    key
-                    valenceKey
-                  }
-                  ... on TimeUsageCellContent {
-                    timeUsageMaxRelevant: maxRelevant
-                    timeUsagePrimaryWatermark: primaryWatermark
-                    key
-                    valenceKey
-                  }
-                  ... on MemoryUsageCellContent {
-                    memoryUsageMaxRelevant: maxRelevant
-                    memoryUsagePrimaryWatermark: primaryWatermark
-                    key
-                    valenceKey
-                  }
-                }
-              }
-            }
-          }
+  fragment FileFragment on FileVariant {
+    name
+    type
+    content {
+      base64
+    }
+  }
+
+  fragment AttachmentFragment on Attachment {
+    title { ...TextFragment }
+    file { ...FileFragment }
+  }
+
+  fragment FieldFragment on Field {
+    id
+    title { ...TextFragment }
+    types {
+      id
+      title { ...TextFragment }
+      extensions
+    }
+  }
+
+  fragment FormFragment on Form {
+    fields { ...FieldFragment }
+  }
+
+  fragment AwardFragment on Award {
+    name
+    title { ...TextFragment }
+    content {
+      __typename
+      ... on ScoreAwardContent {
+        range {
+          ...ScoreRangeFragment
         }
       }
     }
   }
+
+  fragment TableSectionFragment on TableSection {
+    caption { ...TextFragment }
+    cols {
+      title { ...TextFragment }
+      content {
+        __typename
+        ... on ScoreColContent {
+          range { ...ScoreRangeFragment }
+        }
+      }
+    }
+    rowGroups { ...RowGroupFragment }
+  }
+
+  fragment RowNumberCellContentFragment on RowNumberCellContent {
+    number
+  }
+
+  fragment RowTitleCellContentFragment on RowTitleCellContent {
+    title { ...TextFragment }
+  }
+
+  fragment ScoreCellContentFragment on ScoreCellContent {
+    key
+    range { ...ScoreRangeFragment }
+  }
+
+  fragment MessageCellContentFragment on MessageCellContent {
+    key
+    valenceKey
+  }
+
+  fragment TimeUsageCellContentFragment on TimeUsageCellContent {
+    timeUsageMaxRelevant: maxRelevant
+    timeUsagePrimaryWatermark: primaryWatermark
+    key
+    valenceKey
+  }
+
+  fragment MemoryUsageCellContentFragment on MemoryUsageCellContent {
+    memoryUsageMaxRelevant: maxRelevant
+    memoryUsagePrimaryWatermark: primaryWatermark
+    key
+    valenceKey
+  }
+
+  fragment ScoreRangeFragment on ScoreRange {
+    max
+    precision
+  }
+
+  fragment RowGroupFragment on RowGroup {
+    title { ...TextFragment }
+    rows {
+      content
+      cells {
+        content {
+          __typename
+          ... on RowNumberCellContent { ...RowNumberCellContentFragment }
+          ... on RowTitleCellContent { ...RowTitleCellContentFragment }
+          ... on ScoreCellContent { ...ScoreCellContentFragment }
+          ... on MessageCellContent { ...MessageCellContentFragment }
+          ... on TimeUsageCellContent { ...TimeUsageCellContentFragment }
+          ... on MemoryUsageCellContent { ...MemoryUsageCellContentFragment }
+        }
+      }
+    }
+  }
+
+  fragment FeedbackSectionFragment on Section {
+    __typename
+    ... on TableSection { ...TableSectionFragment }
+  }
+
+  fragment MaterialFragment on Material {
+    title { ...TextFragment }
+    statement { ...FileFragment }
+    attachments { ...AttachmentFragment }
+    submissionForm { ...FormFragment }
+    awards { ...AwardFragment }
+    feedback { ...FeedbackSectionFragment }
+  }
+
+  fragment ProblemMaterialFragment on Problem {
+    material { ...MaterialFragment }
+  }
+
   ${textFragment}
-  ${scoreRangeFragment}
 `;
 
 export const scoreRanges = (problem: ProblemMaterialFragment) => problem.material.awards.map(({ name, content }) => {
--- web/projects/turingarena-contest/src/app/score.ts
+++
@@ -1,8 +0,0 @@
-import gql from 'graphql-tag';
-
-export const scoreRangeFragment = gql`
-  fragment ScoreRangeFragment on ScoreRange {
-    precision
-    max
-  }
-`;
