0a4d219568dd05cbc67ef421bbec8dc4e924285c
Massimo Cairo <cairomassimo@gmail.com>
2019-11-03 11:36:42 +0100

    start.sh: fix missing invalid problems dir

--- rust/turingarena-contest/examples/example-contest/start.sh
+++ rust/turingarena-contest/examples/example-contest/start.sh
@@ -6,6 +6,6 @@ for d in easy*/ ; do
     ( cd $d/testo/ && rm -f testo.pdf && latexmk -pdf testo.tex )
 done
 rm database.sqlite3
-cargo run --package turingarena-contest-cli -- init-db
-cargo run --package turingarena-contest-cli -- import-file contest.yaml
-cargo run --features web-content -- serve --secret-key secret
+cargo run --package turingarena-contest-cli -- --problems-dir $PWD init-db
+cargo run --package turingarena-contest-cli -- --problems-dir $PWD import-file contest.yaml
+cargo run --features web-content -- --problems-dir $PWD serve --secret-key secret
