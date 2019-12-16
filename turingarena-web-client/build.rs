85cbc780fa220e23519a4cab17f148ef0dce7319
Massimo Cairo <cairomassimo@gmail.com>
2019-12-16 19:46:48 +0100

    Only build web client if env var is set

--- turingarena-web-client/build.rs
+++ turingarena-web-client/build.rs
@@ -1,10 +1,11 @@
 use std::env;
+use std::error::Error;
 use std::fs::{DirEntry, File};
+use std::io::Write;
 use std::path::Path;
 use std::process::Command;
+
 use turingarena_core::contest::graphql_schema::generate_schema;
-use std::io::Write;
-use std::error::Error;
 
 trait CheckedCommand {
     fn check(&mut self) -> Result<(), Box<dyn Error>>;
@@ -13,7 +14,7 @@ trait CheckedCommand {
 impl CheckedCommand for Command {
     fn check(&mut self) -> Result<(), Box<dyn Error>> {
         if !self.status().unwrap().success() {
-            return Err(format!("command {:?} failed", self).into())
+            return Err(format!("command {:?} failed", self).into());
         }
         Ok(())
     }
@@ -39,40 +40,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     let src_dir = env::var("CARGO_MANIFEST_DIR")?;
     let src_path = Path::new(&src_dir);
 
-    traverse_dir(src_path.join("web").join("projects"), |x| {
-        println!("cargo:rerun-if-changed={}", x.path().to_str().unwrap());
-    });
-
-    {
-        let mut options = fs_extra::dir::CopyOptions::new();
-        options.overwrite = true;
-        fs_extra::dir::copy(src_path.join("web"), out_path, &options)?;
-    }
-
     let out_web_path = out_path.join("web");
 
-    Command::new("npm")
-        .current_dir(&out_web_path)
-        .args(&["ci", "--ignore-scripts"])
-        .check()?;
+    let build_web_env = "TURINGARENA_BUILD_WEB_CLIENT";
+    println!("cargo:rerun-if-env-changed={}", build_web_env);
 
-    std::fs::create_dir_all(&out_web_path.join("__generated__"))?;
+    if env::var_os(build_web_env).is_some() {
+        traverse_dir(src_path.join("web").join("projects"), |x| {
+            println!("cargo:rerun-if-changed={}", x.path().to_str().unwrap());
+        });
 
-    let schema_path = out_web_path
-        .join("__generated__")
-        .join("graphql-schema.json");
-    let mut schema_file = File::create(&schema_path)?;
-    write!(schema_file, "{}", generate_schema())?;
+        {
+            let mut options = fs_extra::dir::CopyOptions::new();
+            options.overwrite = true;
+            fs_extra::dir::copy(src_path.join("web"), out_path, &options)?;
+        }
 
-    Command::new("npm")
-        .current_dir(&out_web_path)
-        .args(&["run", "prepare"])
-        .check()?;
+        Command::new("npm")
+            .current_dir(&out_web_path)
+            .args(&["ci", "--ignore-scripts"])
+            .check()?;
 
-    Command::new("npm")
-        .current_dir(&out_web_path)
-        .args(&["run", "build"])
-        .check()?;
+        std::fs::create_dir_all(&out_web_path.join("__generated__"))?;
+
+        let schema_path = out_web_path
+            .join("__generated__")
+            .join("graphql-schema.json");
+        let mut schema_file = File::create(&schema_path)?;
+        write!(schema_file, "{}", generate_schema())?;
+
+        Command::new("npm")
+            .current_dir(&out_web_path)
+            .args(&["run", "prepare"])
+            .check()?;
+
+        Command::new("npm")
+            .current_dir(&out_web_path)
+            .args(&["run", "build"])
+            .check()?;
+    } else {
+        let dist_path = out_web_path.join("dist").join("turingarena-contest");
+        let index_path = dist_path.join("index.html");
+        std::fs::create_dir_all(dist_path)?;
+        if !index_path.exists() {
+            std::fs::copy(src_path.join("dummy-index.html"), index_path)?;
+        }
+    }
 
     Ok(())
 }
