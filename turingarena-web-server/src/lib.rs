d15b7732a8cf0e15eb2e947ac63c4c62c9e52822
alerighi <alerighi4@gmail.com>
2020-01-04 18:15:17 +0100

    Make turingarena-web-client an optional dependency
    If not, search files in /usr/share/turingarena/web

--- turingarena-web-server/src/lib.rs
+++ turingarena-web-server/src/lib.rs
@@ -7,8 +7,9 @@ use rocket::request::{self, FromRequest, Request};
 use rocket::response::content;
 use rocket::response::Response;
 use rocket::State;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
+#[cfg(feature = "embed")]
 use turingarena_web_client::WebContent;
 
 use rocket::http::ContentType;
@@ -98,16 +99,11 @@ fn post_graphql_handler(
 
 #[rocket::get("/")]
 fn index<'r>() -> rocket::response::Result<'r> {
-    dist(Some(PathBuf::from("index.html")))
+    dist(PathBuf::from("index.html"))
 }
 
-#[rocket::get("/<file_option..>")]
-fn dist<'r>(file_option: Option<PathBuf>) -> rocket::response::Result<'r> {
-    let file = file_option.unwrap_or(PathBuf::new());
-    let filename = file.display().to_string();
-    let content = WebContent::get(&filename)
-        .or(WebContent::get("index.html"))
-        .unwrap();
+#[rocket::get("/<file..>")]
+fn dist<'r>(file: PathBuf) -> rocket::response::Result<'r> {
     let ext = file
         .as_path()
         .extension()
@@ -117,8 +113,23 @@ fn dist<'r>(file_option: Option<PathBuf>) -> rocket::response::Result<'r> {
         .ok_or(Status::new(400, "Could not get file content type"))?;
     Response::build()
         .header(content_type)
-        .sized_body(Cursor::new(content))
+        .sized_body(Cursor::new(get_content(&file)))
         .ok()
+}
+
+#[cfg(feature = "embed")]
+fn get_content(file: &Path) -> Vec<u8> {
+    let filename = file.display().to_string();
+    Vec::from(WebContent::get(&filename)
+        .or(WebContent::get("index.html")).unwrap())
+}
+
+#[cfg(not(feature = "embed"))]
+fn get_content(file: &Path) -> Vec<u8> {
+    let base = Path::new("/usr/share/turingarena/web");
+    std::fs::read(base.join(file))
+        .or(std::fs::read(base.join("index.html")))
+        .unwrap()
 }
 
 /// Run the server
