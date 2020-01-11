f739050446855f11c7ed261471557c5255037ef5
Massimo Cairo <cairomassimo@gmail.com>
2020-01-11 16:33:26 +0100

    Force Python 3.7 in `cli.sh`

--- cli.sh
+++ cli.sh
@@ -7,9 +7,9 @@ VENV="$PWD/.turingarena_venv"
 CLI_DIR="$PWD/python-turingarena-cli/"
 
 if ! [ -d "$VENV" ]; then
-	python3 -m venv "$VENV"
+	python3.7 -m venv "$VENV"
 	. "$VENV/bin/activate"
-	(cd "$CLI_DIR" && python3 setup.py develop)
+	(cd "$CLI_DIR" && python3.7 setup.py develop)
 	
 fi
 
