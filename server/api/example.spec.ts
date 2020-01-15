411d9fed0474b7abba6dd804e22e93378c5423df
Massimo Cairo <cairomassimo@gmail.com>
2020-01-15 19:52:50 +0100

    server: generate types for local operations

--- server/api/example.spec.ts
+++ server/api/example.spec.ts
@@ -1,13 +1,19 @@
 import { gql } from 'apollo-server-core';
-import { localClient } from '../helpers/local';
+import { execute } from 'graphql';
+import { executableSchema } from '.';
+import { InitMutation } from '../generated/graphql-types';
 
 it('should init', async () => {
-    const response = await localClient.mutate({
-        mutation: gql`
-            mutation {
-                init
+    const response = await execute<InitMutation>(
+        executableSchema,
+        gql`
+            mutation Init {
+                init {
+                    __typename
+                }
             }
         `,
-    });
-    expect(response.data.init).toBeDefined();
+    );
+
+    expect(response.data?.init).toBeDefined();
 });
