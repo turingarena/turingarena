b63b2fb2bcd3ef71874c8b8c6ed6e721774c1fe3
Massimo Cairo <cairomassimo@gmail.com>
2020-01-20 21:23:01 +0100

    web: remove most non-working parts

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyComponent } from './empty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmptyComponent,
+    children: [
+      {
+        path: '',
+        component: EmptyComponent,
+      },
+      {
+        path: 'problem/:problemName',
+        component: EmptyComponent,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    component: EmptyComponent,
+    children: [
+      {
+        path: '',
+        component: EmptyComponent,
+        data: {
+          adminSection: '',
+        },
+      },
+      {
+        path: 'problems',
+        component: EmptyComponent,
+        data: {
+          adminSection: 'problems',
+        },
+      },
+      {
+        path: 'contestants',
+        component: EmptyComponent,
+        data: {
+          adminSection: 'contestants',
+        },
+      },
+      {
+        path: 'messages',
+        component: EmptyComponent,
+        data: {
+          adminSection: 'messages',
+        },
+      },
+      {
+        path: 'submissions',
+        component: EmptyComponent,
+        data: {
+          adminSection: 'submissions',
+        },
+      },
+      {
+        path: 'evaluations',
+        component: EmptyComponent,
+        data: {
+          adminSection: 'evaluations',
+        },
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
