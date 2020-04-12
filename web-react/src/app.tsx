92ae7e37260284d449cf3f3eb729ca01508f0f83
Massimo Cairo <cairomassimo@gmail.com>
2020-04-12 18:05:52 +0200

    web-react: load main query

--- web-react/src/app.tsx
+++ web-react/src/app.tsx
@@ -1,7 +1,7 @@
-import { ApolloClient, ApolloProvider, gql, HttpLink, InMemoryCache, useQuery } from '@apollo/client';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import './app.css'; // tslint:disable-line: no-import-side-effect
-import { default as logo } from './logo.svg'; // tslint:disable-line: no-default-import
+import { MainLoader } from './core/main-loader';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -10,38 +10,10 @@ const client = new ApolloClient({
   }),
 });
 
-export function MainView() {
-  const { loading, error, data } = useQuery(gql`
-    query Main {
-      mainView(username: "user1") {
-        user {
-          id
-        }
-      }
-    }
-  `);
-
-  if (loading) return <>Loading...</>;
-  if (error !== undefined) return <>Error! {error.message}</>;
-
-  return <div>{JSON.stringify(data)}</div>;
-}
-
 export function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <MainView></MainView>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MainLoader />
     </ApolloProvider>
   );
 }
