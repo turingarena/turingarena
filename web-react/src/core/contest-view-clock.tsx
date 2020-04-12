4a20d45bdf7e027f0a051b9bc45462a59f3b1cb4
William Di Luigi <williamdiluigi@gmail.com>
2020-04-12 21:23:14 +0200

    contest-view-clock: New component structure

--- web-react/src/core/contest-view-clock.tsx
+++ web-react/src/core/contest-view-clock.tsx
@@ -1,4 +1,11 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
 import { gql } from '@apollo/client';
+import { useObservable } from "rxjs-hooks";
+import { DateTime, Duration } from 'luxon';
+import { interval } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+import { ContestViewClockFragment, ContestStatus } from '../generated/graphql-types';
 
 export const contestViewClockFragment = gql`
   fragment ContestViewClock on ContestView {
@@ -9,3 +16,82 @@ export const contestViewClockFragment = gql`
     }
   }
 `;
+
+const ContestClock = styled.div<{status: ContestStatus}>`
+  font-family: "Lucida Console", Monaco, monospace;
+  font-size: 2rem;
+  text-align: right;
+  margin-bottom: 16px;
+
+  ${({status}) => (status === 'ENDED' && css`
+    /* TODO */
+    /* @extend .text-danger; */
+  `)}
+`;
+
+const getContestStatus = (status: ContestStatus) => {
+  switch (status) {
+    case 'NOT_STARTED':
+      return 'Starting In';
+    case 'RUNNING':
+      return 'Remaining Time';
+    case 'ENDED':
+      return 'Ended';
+  }
+};
+
+// TODO: duplicated?
+const AsideHeader = styled.h2`
+  text-transform: uppercase;
+  font-size: 1.25rem;
+  margin: 0 0 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
+`;
+
+export function ContestViewClock({data}: {data: ContestViewClockFragment}) {
+  const clock = useObservable(() => interval(Duration.fromObject({ seconds: 1 }).as('milliseconds')).pipe(
+    startWith(0),
+    map(() => {
+      const now = DateTime.local();
+      const start = DateTime.fromISO(data.contest.start);
+      const end = DateTime.fromISO(data.contest.end);
+
+      switch (data.contest.status) {
+        case 'NOT_STARTED':
+          return start.diff(now);
+        case 'RUNNING':
+          return end.diff(now);
+        case 'ENDED':
+          return now.diff(end);
+        default:
+          throw new Error();
+      }
+    }),
+    map(duration =>
+      duration.valueOf() >= 0
+        ? {
+            duration,
+            negated: false,
+          }
+        : {
+            duration: duration.negate(),
+            negated: true,
+          },
+    ),
+  ));
+
+  return (
+    <div>
+      <AsideHeader>
+        {getContestStatus(data.contest.status)}
+      </AsideHeader>
+
+      {clock !== null && (
+        <ContestClock status={data.contest.status}>
+          { clock.negated ? '-' : ''}{ clock.duration.toFormat('hh:mm:ss') }
+        </ContestClock>
+      )}
+    </div>
+  );
+}
