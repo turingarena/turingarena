a4d2cd29f9e00672fc034622d7a1f6c78689a87f
Massimo Cairo <cairomassimo@gmail.com>
2020-04-12 20:01:41 +0200

    Add TopBar component (HTML only)

--- web-react/src/core/top-bar.tsx
+++ web-react/src/core/top-bar.tsx
@@ -1,5 +1,22 @@
 import { gql } from '@apollo/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { TopBarFragment } from '../generated/graphql-types';
+import { FragmentProps } from '../util/fragment-props';
 import { textFragment } from './text';
+
+export function TopBar({ data }: FragmentProps<TopBarFragment>) {
+  return (
+    <nav className="top-bar">
+      <Link to="/">
+        <h1 className="top-bar-title">
+          <FontAwesomeIcon icon="home" /> {data.title.variant}
+        </h1>
+      </Link>
+    </nav>
+  );
+}
 
 export const topBarFragment = gql`
   fragment TopBar on MainView {
