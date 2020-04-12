cf60b494ac38d8acdcc429173b9040c595f25bd6
Massimo Cairo <cairomassimo@gmail.com>
2020-04-12 17:17:48 +0200

    web-react: add and configure Apollo

--- config.tmux
+++ config.tmux
@@ -13,29 +13,37 @@ set-hook pane-exited kill-window #{hook_window} ;
 
 # attach-session ;
 
-split-window -v -p 33 -t 0 -c server/
-split-window -h -t 1 -c web/
+select-layout tiled
 
-split-window -v -t 0 -c server/ npm run start
-# split-window -h -t 1 cat # dummy
-split-window -h -t 1 -c web/ npm run start
-split-window -h -t 2 -c web/ npm run watch:prepare
+split-window -c server/
+select-layout tiled
 
-split-window -v -t 0 -c server/ npm run watch:check:test
-split-window -h -t 1 -c server/ npm run watch:check:tslint
-split-window -h -t 1 -c server/ npm run watch:check:tsc
-split-window -h -t 3 -c server/ npm run watch:prepare:graphql-codegen
+split-window -c web/
+select-layout tiled
 
-kill-pane -t 0
+split-window -c server/ npm run start
+select-layout tiled
 
-# split-window -h -t 0 -l 1 cat # -c server/
-# split-window -h -t 0 -l 1 cat # -c web/
-# split-window -h -t 0 -l 1 cat # dummy
-# split-window -h -t 0 -l 1 cat # dummy
+split-window -c web/ npm run start
+select-layout tiled
 
-# kill-pane -t 0
+split-window -c web/ npm run watch:prepare
+select-layout tiled
 
-# split-window -h -c server/ npm run watch:lint ;
-# split-window -h -c web/ npm run start ;
+split-window -c web-react/ npm run start
+select-layout tiled
 
-# select-layout tiled ;
+split-window -c web-react/ npm run watch:prepare
+select-layout tiled
+
+split-window -c server/ npm run watch:check:test
+select-layout tiled
+
+split-window -c server/ npm run watch:check:tslint
+select-layout tiled
+
+split-window -c server/ npm run watch:check:tsc
+select-layout tiled
+
+split-window -c server/ npm run watch:prepare:graphql-codegen
+select-layout tiled
--- web-react/src/app.tsx
+++ web-react/src/app.tsx
@@ -1,19 +1,47 @@
+import { ApolloClient, ApolloProvider, gql, HttpLink, InMemoryCache, useQuery } from '@apollo/client';
 import React from 'react';
 import './app.css'; // tslint:disable-line: no-import-side-effect
 import { default as logo } from './logo.svg'; // tslint:disable-line: no-default-import
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: '/graphql',
+  }),
+});
+
+export function MainView() {
+  const { loading, error, data } = useQuery(gql`
+    query Main {
+      mainView(username: "user1") {
+        user {
+          id
+        }
+      }
+    }
+  `);
+
+  if (loading) return <>Loading...</>;
+  if (error !== undefined) return <>Error! {error.message}</>;
+
+  return <div>{JSON.stringify(data)}</div>;
+}
+
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <MainView></MainView>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
