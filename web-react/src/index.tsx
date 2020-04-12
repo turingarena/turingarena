836bb6a7e4ec8105f1999dfed482c1c1a4277e6d
alerighi <alerighi4@gmail.com>
2020-04-12 22:18:50 +0200

    Merge app.tsx in index.tsx

--- web-react/src/index.tsx
+++ web-react/src/index.tsx
@@ -1,19 +1,28 @@
 // tslint:disable: no-import-side-effect no-submodule-imports
-
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './core/app';
+import { MainLoader } from './core/main-loader';
 import './index.css';
 
 library.add(fas);
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: '/graphql',
+  }),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <MainLoader />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
