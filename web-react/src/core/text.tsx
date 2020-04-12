87a86f90487e5c6bb90e9a2f50ecb492de7f8549
alerighi <alerighi4@gmail.com>
2020-04-12 23:15:54 +0200

    Implement Text component

--- web-react/src/core/text.tsx
+++ web-react/src/core/text.tsx
@@ -1,4 +1,11 @@
 import { gql } from '@apollo/client';
+import React from 'react';
+import { TextFragment } from '../generated/graphql-types';
+import { FragmentProps } from '../util/fragment-props';
+
+export function Text({ data }: FragmentProps<TextFragment>) {
+  return <>{data.variant}</>;
+}
 
 export const textFragment = gql`
   fragment Text on Text {
