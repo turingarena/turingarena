cb09b5a06bc2abb4c3ea6244dea70ab14aa631bb
Massimo Cairo <cairomassimo@gmail.com>
2020-04-13 20:30:39 +0200

    Use credentials after log-in

--- web-react/src/index.tsx
+++ web-react/src/index.tsx
@@ -1,29 +1,53 @@
 // tslint:disable: no-import-side-effect no-submodule-imports no-default-import
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, concat, HttpLink, InMemoryCache } from '@apollo/client';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CurrentAuthProvider, currentAuthQuery, initialCurrentAuthQueryData } from './core/current-auth';
 import { MainLoader } from './core/main-loader';
+import { CurrentAuthQuery, CurrentAuthQueryVariables } from './generated/graphql-types';
 import result from './generated/possible-types';
 import './index.css';
 
 library.add(fas);
 
-const client = new ApolloClient({
-  cache: new InMemoryCache({ possibleTypes: result.possibleTypes }),
-  link: new HttpLink({
-    uri: '/graphql',
-  }),
+const cache = new InMemoryCache({ possibleTypes: result.possibleTypes });
+cache.writeQuery<CurrentAuthQuery, CurrentAuthQueryVariables>({
+  query: currentAuthQuery,
+  data: initialCurrentAuthQueryData,
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <MainLoader />
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const client = new ApolloClient({
+  cache,
+  link: concat(
+    (operation, forward) => {
+      const data = cache.readQuery<CurrentAuthQuery, CurrentAuthQueryVariables>({ query: currentAuthQuery });
+      const token = data?.currentToken ?? null;
+
+      if (token !== null) {
+        operation.setContext({ headers: { authorization: `Bearer ${token}` } });
+      }
+
+      return forward(operation);
+    },
+    new HttpLink({
+      uri: '/graphql',
+    }),
+  ),
+  resolvers: {}, // allow local queries
+});
+
+function App() {
+  return (
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <CurrentAuthProvider component={currentAuth => <MainLoader currentAuth={currentAuth} />} />
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
