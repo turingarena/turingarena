3b31e96c831d41d4d26f3dccc2b3f5e81de5bb5e
William Di Luigi <williamdiluigi@gmail.com>
2020-04-13 14:02:29 +0200

    modal: Add draft

---
+++ web-react/src/util/components/modal.tsx
@@ -0,0 +1,38 @@
+import { css } from 'emotion';
+import React from 'react';
+
+interface Props {
+  onClose: () => void;
+  children: React.ReactElement;
+}
+
+export function Modal({ onClose, children }: Props) {
+  return (
+    <div>
+      <div
+        onClick={onClose}
+        className={css`
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          z-index: 100;
+          background-color: rgba(0, 0, 0, 0.3);
+        `}
+      />
+
+      <div
+        className={css`
+          position: fixed;
+          top: 100px;
+          left: 400px;
+          right: 400px;
+          bottom: 100px;
+        `}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
