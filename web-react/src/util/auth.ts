91ffa36faf31fce8bbd43b7dcc97a1d603cf2ecb
alerighi <alerighi4@gmail.com>
2020-04-14 20:45:49 +0200

    Simplify useAuth hook

--- web-react/src/util/auth.ts
+++ web-react/src/util/auth.ts
@@ -1,40 +1,34 @@
-import { ApolloClient, gql, useApolloClient } from '@apollo/client';
+import { gql, useApolloClient } from '@apollo/client';
 import { CurrentAuthWriteQuery, CurrentAuthWriteQueryVariables } from '../generated/graphql-types';
 
-const authQuery = gql`
-  query CurrentAuthWrite {
-    currentToken @client
-    currentUsername @client
-  }
-`;
+export function useAuth() {
+  const client = useApolloClient();
 
-class Auth {
-  constructor(private readonly client: ApolloClient<object>) {}
-
-  private authQuery(token: string | null, username: string | null) {
+  const authQuery = (token: string | null, username: string | null) => {
     // FIXME: breaks AgGrid
     // await client.resetStore();
-    this.client.writeQuery<CurrentAuthWriteQuery, CurrentAuthWriteQueryVariables>({
-      query: authQuery,
+    client.writeQuery<CurrentAuthWriteQuery, CurrentAuthWriteQueryVariables>({
+      query: gql`
+        query CurrentAuthWrite {
+          currentToken @client
+          currentUsername @client
+        }
+      `,
       data: {
         __typename: 'Query',
         currentToken: token,
         currentUsername: username,
       },
     });
-  }
+  };
 
-  setAuth({ token, username }: { token: string; username: string }) {
-    this.authQuery(token, username);
-  }
+  const setAuth = ({ token, username }: { token: string; username: string }) => {
+    authQuery(token, username);
+  };
 
-  clearAuth() {
-    this.authQuery(null, null);
-  }
-}
+  const clearAuth = () => {
+    authQuery(null, null);
+  };
 
-export function useAuth() {
-  const client = useApolloClient();
-
-  return new Auth(client);
+  return { setAuth, clearAuth };
 }
