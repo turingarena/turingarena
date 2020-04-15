68f093e60fda75939e573032646527f3ed4e4053
Massimo Cairo <cairomassimo@gmail.com>
2020-04-15 21:20:43 +0200

    Add prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 set -ex
 
 ( cd server/ ; npm ci )
 ( cd web/ ; npm ci )
+
+if [ -f server/db.sqlite3 ] ; then
+    mv server/db.sqlite3 server/db.sqlite3.bak
+fi
+
+( cd server/ ; npm run cli -- import ../examples/example-contest/ )
