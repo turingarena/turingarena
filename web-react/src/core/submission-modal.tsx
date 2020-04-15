006672488ba7e0afcaf5bab5969bf13543039325
Massimo Cairo <cairomassimo@gmail.com>
2020-04-15 16:12:53 +0200

    Implement submission modal (no refresh yet)

--- web-react/src/core/submission-modal.tsx
+++ web-react/src/core/submission-modal.tsx
@@ -1,11 +1,15 @@
 import { gql } from '@apollo/client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { AgGridReact } from 'ag-grid-react';
+import { css, cx } from 'emotion';
 import React from 'react';
 import { RecordFragment, SubmissionModalFragment } from '../generated/graphql-types';
+import { buttonCss, buttonPrimaryCss } from '../util/components/button';
+import { gridCss } from '../util/components/grid';
+import { modalBodyCss, modalFooterCss, modalHeaderCss } from '../util/components/modal';
 import { FragmentProps } from '../util/fragment-props';
 import { columnFragment, getFieldColumns, recordFragment } from './field-table';
 import { textFragment } from './text';
-import { css } from 'emotion';
 
 export const submissionModalFragment = gql`
   fragment SubmissionModal on Submission {
@@ -40,17 +44,47 @@ export const submissionModalFragment = gql`
 
 export function SubmissionModal({ data }: FragmentProps<SubmissionModalFragment>) {
   return (
-    <div
-      className={css`
-        width: 80vw;
-        overflow: auto;
-      `}
-    >
-      <AgGridReact
-        columnDefs={getFieldColumns(data.feedbackTable.columns, (row: RecordFragment) => row)}
-        domLayout="autoHeight"
-        rowData={data.feedbackTable.rows}
-      />
-    </div>
+    <>
+      <div
+        className={css`
+          width: 80vw;
+          overflow: auto;
+        `}
+      >
+        <div className={modalHeaderCss}>
+          <h5>
+            {/* TODO: submission index, e.g., Submission #6 for: My Problem */}
+            Submission for: <strong>{data.problem.title.variant}</strong>
+          </h5>
+        </div>
+        <div
+          className={
+            data.officialEvaluation?.status === 'PENDING'
+              ? css`
+                  background-color: rgba(0, 0, 0, 0.1);
+                  text-align: center;
+                `
+              : css`
+                  display: none;
+                `
+          }
+        >
+          <FontAwesomeIcon icon="spinner" pulse /> Evaluating...
+        </div>
+
+        <div className={cx(gridCss)}>
+          <AgGridReact
+            columnDefs={getFieldColumns(data.feedbackTable.columns, (row: RecordFragment) => row)}
+            domLayout="autoHeight"
+            rowData={data.feedbackTable.rows}
+          />
+        </div>
+        <div className={modalFooterCss}>
+          <button onClick={() => {}} className={cx(buttonCss, buttonPrimaryCss)}>
+            Close
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ web-react/src/util/components/grid.tsx
@@ -0,0 +1,1 @@
+export const gridCss = 'ag-theme-balham';
