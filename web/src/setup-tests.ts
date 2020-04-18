0c880caded327910afd1972580020f298d983d26
alerighi <alerighi4@gmail.com>
2020-04-18 15:41:37 +0200

    Update install script

--- web/src/setup-tests.ts
+++ web/src/setup-tests.ts
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// tslint:disable-next-line: no-implicit-dependencies
 import '@testing-library/jest-dom/extend-expect'; // tslint:disable-line: no-import-side-effect no-submodule-imports
