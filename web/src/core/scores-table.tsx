7485d2ed458a964e1d301703ad52357c1e912c13
djsdvg <leonardo.ballini@protonmail.com>
2021-02-04 02:33:03 -0800

    Refactoring coloring cells  rules. Fixed a bug that made the browser crash when a user who is not an admin tries to use the dashboard

--- web/src/core/scores-table.tsx
+++ web/src/core/scores-table.tsx
@@ -55,36 +55,32 @@ export function ScoresTable() {
   }
 
   const ragCellClassRules = {
-    'rag-green': (params: any) => {
-      return parseInt(params.value, 10) === 100
-    },
-    'rag-amber':(params: { value: string })=> {
-      return parseInt(params.value, 10) < 100 && parseInt(params.value, 10) > 50;
-    },
-    'rag-red':(params: { value: string })=> {
-      return parseInt(params.value, 10) <= 50;
-    },
+    'rag-green': (params: any) => parseInt(params.value, 10) === 100,
+    'rag-amber': (params: { value: string }) => parseInt(params.value, 10) < 100 && parseInt(params.value, 10) > 50,
+    'rag-red': (params: { value: string }) => parseInt(params.value, 10) <= 50,
   };
 
   useEffect(() => {
-    const tmpproblemnames: string[] = [];
-    data?.contests[0].problemSet.assignments.forEach((a: { problem: { name: any } }) =>
-      tmpproblemnames.push(a.problem.name),
-    );
-    setProblemsNames(tmpproblemnames);
+    if (data?.contests[0] !== undefined) {
+      const tmpproblemnames: string[] = [];
+      data?.contests[0].problemSet.assignments.forEach((a: { problem: { name: any } }) =>
+        tmpproblemnames.push(a.problem.name),
+      );
+      setProblemsNames(tmpproblemnames);
 
-    const tmpScores: Scores[] = [];
-    data?.contests[0].problemSet.userTacklings.map((ut: ContestProblemSetUserTackling) => {
-      const toAdd: Scores = { username: '' };
-      toAdd.username = ut.user.username;
-      toAdd.totalScore = ut.totalScoreGrade.score.toString();
-      ut.assignmentTacklings.forEach((at, index) => {
-        toAdd[tmpproblemnames[index]] = at.scoreGrade.score.toString();
-        console.log(tmpproblemnames);
+      const tmpScores: Scores[] = [];
+      data?.contests[0].problemSet.userTacklings.map((ut: ContestProblemSetUserTackling) => {
+        const toAdd: Scores = { username: '' };
+        toAdd.username = ut.user.username;
+        toAdd.totalScore = ut.totalScoreGrade.score.toString();
+        ut.assignmentTacklings.forEach((at, index) => {
+          toAdd[tmpproblemnames[index]] = at.scoreGrade.score.toString();
+          console.log(tmpproblemnames);
+        });
+        tmpScores.push(toAdd);
       });
-      tmpScores.push(toAdd);
-    });
-    setPivotScores(tmpScores);
+      setPivotScores(tmpScores);
+    }
   }, data);
 
   return (
